e7b53b67a6a4e76df03cae3b79c1d7dfc6608e00
Ryan Ruckley <rruckley@gmail.com>
2023-09-15 14:35:39 +1000

    offering mod

--- src/tmf620/mod.rs
+++ src/tmf620/mod.rs
@@ -1,8 +1,9 @@
 //! TMF620 Product Catalogue Module
 
 
-mod tmf620;
-mod catalog;
-mod party;
-mod category;
-mod product_specification;
+pub mod tmf620;
+pub mod catalog;
+pub mod party;
+pub mod category;
+pub mod product_specification;
+pub mod product_offering;
