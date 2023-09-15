04904ba092c44f7bfc1e6af3ae62541f6beadfc5
Ryan Ruckley <rruckley@gmail.com>
2023-09-15 13:30:50 +1000

    new mod

---
+++ src/tmf620/product_specification.rs
@@ -0,0 +1,8 @@
+//! Product Specification Module
+//! 
+
+/// Product Specification Reference
+pub struct ProductSpecificationRef {}
+
+/// Product Specification Characteristic Value Use
+pub struct ProductSpecificationCharacteristicValueUse {}
