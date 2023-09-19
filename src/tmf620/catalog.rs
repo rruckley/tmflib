e7d58230448b2e9df33ac9e6757d38c10f18e877
Ryan Ruckley <rruckley@gmail.com>
2023-09-19 13:32:20 +1000

    fields optional

--- src/tmf620/catalog.rs
+++ src/tmf620/catalog.rs
@@ -4,19 +4,47 @@
 use crate::tmf620::party::RelatedParty;
 use crate::tmf620::category::CategoryRef;
 
+use serde::{Deserialize,Serialize};
+
 /// Catalogue
+#[derive(Deserialize, Serialize)]
 pub struct Catalog {
-    id              : String,
-    href            : String,
-    catalog_type    : String,
-    description     : String,
-    last_upodate    : String,
-    lifecycle_status : String,
-    name            : String,
-    version         : String,
-    valid_for       : String,
+    id              : Option<String>,
+    href            : Option<String>,
+    catalog_type    : Option<String>,
+    description     : Option<String>,
+    last_update     : Option<String>,
+    lifecycle_status : Option<String>,
+    name            : Option<String>,
+    version         : Option<String>,
+    valid_for       : Option<String>,
     /// Categories
-    category        : Vec<CategoryRef>,
+    category        : Option<Vec<CategoryRef>>,
     /// Related parties
-    related_party   : Vec<RelatedParty>,
+    related_party   : Option<Vec<RelatedParty>>,
+}
+
+impl Catalog {
+    /// Create a new instance of catalog struct
+    pub fn new() -> Catalog {
+        Catalog::default()
+    }
+}
+
+impl std::default::Default for Catalog {
+    fn default() -> Catalog {
+        Catalog {
+            id          : None,
+            href        : None,
+            catalog_type : None,
+            description : None,
+            last_update : None,
+            lifecycle_status : None,
+            name        : None,
+            version     : None,
+            valid_for   : None,
+            category    : None,
+            related_party : None,
+        }
+    }
 }
