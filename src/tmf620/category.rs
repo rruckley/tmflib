179e474fe0e8e10ba80f0b83a05746d2cd0cf5d5
Ryan Ruckley <rruckley@gmail.com>
2023-09-20 11:36:30 +1000

    tests

--- src/tmf620/category.rs
+++ src/tmf620/category.rs
@@ -8,6 +8,7 @@ use uuid::Uuid;
 use super::MOD_PATH;
 
 const CAT_PATH : &str = "catalog";
+const CAT_VERS : &str = "1.0";
 
 /// Category Resource
 #[derive(Debug,Deserialize, Serialize)]
@@ -18,7 +19,7 @@ pub struct Category {
     /// HREF where object is located
     pub href : Option<String>,
     /// Description
-    pub description : Option<String>,
+    description : Option<String>,
     /// Is this the root of a heirarchy of categories?
     pub is_root : bool,
     /// When was this object last updated?
@@ -44,7 +45,7 @@ impl Category {
     /// # Examples
     /// ```
     /// # use tmflib::tmf620::category::Category;
-    /// let cat = Category::new();
+    /// let cat = Category::new(String::from("MyCategory"));
     /// ```
     pub fn new(name : String) -> Category {
         let id = Uuid::new_v4().to_string();
@@ -53,35 +54,23 @@ impl Category {
             id          : Some(id),
             href        : Some(href),
             description : None,
-            is_root     : false,
+            is_root     : true,
             last_update : None,
             lifecycle_status : None,
             name        : Some(name.clone()),
             parent_id   : None,
-            version     : None,
+            version     : Some(CAT_VERS.to_string()),
             valid_for   : None,
             sub_category: None, 
             product_offering: None
         }
     }
 
-    /// Set the name
-    /// # Examples
-    /// ```
-    /// # use tmflib::tmf620::category::Category;
-    /// let cat = Category::new()
-    ///     .name(String::from("Components"));
-    /// ```
-    pub fn name(mut self, name : String) -> Category {
-        self.name = Some(name);
-        self
-    }
-
     /// Set the description of this category
     /// # Examples
     /// ```
     /// # use tmflib::tmf620::category::Category;
-    /// let cat = Category::new()
+    /// let cat = Category::new(String::from("MyCategory"))
     ///     .description(String::from("Library of product components"));
     /// ```
     pub fn description(mut self, description :  String) -> Category {
@@ -89,6 +78,68 @@ impl Category {
         self
     }
 
+    /// Set parent category
+    /// # Examples
+    /// ```
+    /// # use tmflib::tmf620::category::Category;
+    /// let cat = Category::new(String::from("MyCategory"))
+    ///     .parent(String::from("23948-234908"));
+    /// ```
+    pub fn parent(mut self, parent_id : String) -> Category {
+        // Since we are setting a parent, we cannot be root anymore
+        self.is_root = false;
+        self.parent_id = Some(parent_id);
+        self
+    }
+
+    /// Set is_root, will remove parent linkage if true.
+    /// # Examples
+    /// Setting is_root=true will also set parent_id to None.
+    /// ```
+    /// # use tmflib::tmf620::category::Category;
+    /// let cat = Category::new(String::from("MyCategory"))
+    ///     .is_root(true);
+    /// ```
+    pub fn is_root(mut self, root : bool) -> Category {
+        self.is_root = root;
+        if self.is_root {
+            // Remove parent
+            self.parent_id = None;
+        }
+        self
+    }
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Category;
+    use super::CAT_VERS;
+    #[test]
+    fn cat_test_name() {
+        let cat = Category::new(String::from("MyCategory"));
+        assert_eq!(cat.name, Some(String::from("MyCategory")))
+    }
+
+    #[test]
+    fn cat_test_version() {
+        let cat = Category::new(String::from("MyCategory"));
+        assert_eq!(cat.version, Some(CAT_VERS.to_string()));
+    }
+
+    #[test]
+    fn cat_test_root() {
+        let cat = Category::new(String::from("MyCategory"));
+        assert_eq!(cat.is_root, true);
+    }
+
+    #[test]
+    fn cat_test_parent() {
+        let cat = Category::new(String::from("MyCategory"))
+            .parent(String::from("parent_id"));
+        assert_eq!(cat.parent_id,Some(String::from("parent_id")));
+        assert_eq!(cat.is_root, false);
+    }
 }
 
 /// Category Reference
