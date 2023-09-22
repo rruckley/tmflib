66b9176486f4b443fd249de2b3a9c793fea20fbd
Ryan Ruckley <rruckley@gmail.com>
2023-09-22 19:18:06 +1000

    new mod

---
+++ src/tmf629/characteristic.rs
@@ -0,0 +1,12 @@
+//! Customer Characteristic Module
+
+use serde::{Deserialize,Serialize};
+
+/// Customer Characteristic
+#[derive(Clone,Debug,Deserialize,Serialize)]
+pub struct Characteristic {
+    pub name    : String,
+    pub value_type : String,
+    pub value   : String,
+}
+
