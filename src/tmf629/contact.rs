6fdd649db44c9ededbdd2fe430d7e1b74a3c2812
Ryan Ruckley <rruckley@gmail.com>
2023-09-22 19:18:45 +1000

    basic struct

---
+++ src/tmf629/contact.rs
@@ -0,0 +1,30 @@
+//! Contact Module
+//! 
+use serde::{Deserialize,Serialize};
+use std::hash::Hash;
+
+/// Contact Medium
+#[derive(Clone,Debug,Deserialize,Hash,Serialize)]
+pub struct ContactMedium {
+    characteristic : ContactCharacteristic,
+    prefferred      : bool,
+}
+
+impl ContactMedium {
+    pub fn new() -> ContactMedium {
+        ContactMedium {
+            prefferred : false,
+            characteristic : ContactCharacteristic {
+                email_address : None,
+                phone_number  : None,
+            },
+        }
+    }
+}
+
+/// Contact Characteristic
+#[derive(Clone,Debug,Deserialize,Hash,Serialize)]
+pub struct ContactCharacteristic {
+    email_address   : Option<String>,
+    phone_number    : Option<String>,
+}
