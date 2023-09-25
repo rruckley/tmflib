b4bee60bbf1ee784ac25774633da6514f602cd85
Ryan Ruckley <rruckley@gmail.com>
2023-09-25 11:03:54 +1000

    expanded example

--- examples/create_specification.rs
+++ examples/create_specification.rs
@@ -3,7 +3,16 @@ use tmflib::tmf620::product_specification::{ProductSpecification,ProductSpecific
 
 fn main() {
 
-    let char = ProductSpecification::new(String::from("MySpecification"));
+    let char1 = ProductSpecificationCharacteristic::new(String::from("OptionalChar"))
+        .description(String::from("This characteristic is mandatory"));
+    let char2 = ProductSpecificationCharacteristic::new(String::from("MandatoryChar"))
+        .cardinality(1,1)
+        .description(String::from("This is a mandatory characteristic."));
 
-    dbg!(char);
+
+    let spec = ProductSpecification::new(String::from("MySpecification"))
+        .with_charateristic(char1)
+        .with_charateristic(char2);
+
+    dbg!(spec);
 }
