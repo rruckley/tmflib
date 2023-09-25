b03ab76fb74c9a0bd3d69f9537144d3b533f886b
Ryan Ruckley <rruckley@gmail.com>
2023-09-25 15:04:12 +1000

    new mod

---
+++ src/tmf620/bundled_product_offering.rs
@@ -0,0 +1,50 @@
+//! Bundled Product Offering Module
+
+const BUNDLE_PATH : &str = "bundle";
+use super::product_offering::ProductOffering;
+use super::LIB_PATH;
+use super::MOD_PATH;
+
+use serde::{Deserialize,Serialize};
+
+/// Bundled Product Offering details
+#[derive(Clone,Debug,Deserialize,Serialize)]
+pub struct BundledProductOffering {
+    pub bundled_product_offering_option : Option<BundledProductOfferingOption>,
+    pub offer : ProductOffering,
+}
+
+impl BundledProductOffering {
+    pub fn new(name : String) -> BundledProductOffering {
+        let mut offer = ProductOffering::new(name);
+        // Update href to point to bundle instead of stadnard offer path
+        let href = format!("/{}/{}/{}/{}",LIB_PATH,MOD_PATH,BUNDLE_PATH,offer.id.as_ref().unwrap());
+        offer.href = Some(href.clone());
+        BundledProductOffering {
+            offer,
+            bundled_product_offering_option : None,
+        }
+    }
+
+    pub fn with_option(mut self, option : BundledProductOfferingOption) -> BundledProductOffering {
+        self.bundled_product_offering_option = Some(option);
+        self
+    }
+}
+
+#[derive(Clone,Debug,Default,Deserialize,Serialize)]
+pub struct BundledProductOfferingOption {
+    number_rel_offer_default    : u8,
+    number_rel_offer_lower_limit    : u8,
+    number_rel_offer_upper_limit    : u8,
+}
+
+impl BundledProductOfferingOption {
+    pub fn new(default : u8, min : u8, max: u8) -> BundledProductOfferingOption {
+        BundledProductOfferingOption { 
+            number_rel_offer_default:  
+            default, number_rel_offer_lower_limit: 
+            min, number_rel_offer_upper_limit: max
+        }
+    }
+}
