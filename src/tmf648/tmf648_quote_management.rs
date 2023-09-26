fbbb1156832382f5f63896257789c16c9c183a89
Ryan Ruckley <rruckley@gmail.com>
2023-09-26 12:05:08 +1000

    new tmf648 mod

---
+++ src/tmf648/tmf648_quote_management.rs
@@ -0,0 +1,22 @@
+
+use serde::{Deserialize,Serialize};
+
+use super::quote::Quote;
+
+#[derive(Deserialize,Serialize)]
+pub struct TMF648QuoteManagement {
+    quotes  : Vec<Quote>,
+}
+
+impl TMF648QuoteManagement {
+    pub fn new() -> TMF648QuoteManagement {
+        TMF648QuoteManagement { 
+            quotes: vec![] 
+        }
+    }
+
+    pub fn add_quote(&mut self, quote : Quote) -> Result<String,String> {
+        self.quotes.push(quote);
+        Ok(String::from("Quote Added"))
+    }
+}
