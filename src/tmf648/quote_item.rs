725f341fccd541d0a98314acdb83570c6dbe0cc5
Ryan Ruckley <rruckley@gmail.com>
2023-09-26 16:07:04 +1000

    pull in attachments

--- src/tmf648/quote_item.rs
+++ src/tmf648/quote_item.rs
@@ -3,11 +3,17 @@
 use serde::{Deserialize,Serialize};
 use uuid::Uuid;
 
+use crate::common::attachment::AttachmentRefOrValue;
+
 #[derive(Clone,Debug,Deserialize,Serialize)]
 pub struct QuoteItem {
     id  : String,
+    action : Option<String>,
     quantity : u16,
+    #[serde(skip_serializing_if = "Option::is_none")]
     quote_item : Option<Vec<QuoteItem>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    attachment : Option<Vec<AttachmentRefOrValue>>,
 }
 
 impl QuoteItem {
@@ -15,8 +21,10 @@ impl QuoteItem {
         let id = Uuid::new_v4().to_string();
         QuoteItem { 
             id,
+            action : None,
             quantity : 1,
             quote_item : None,
+            attachment : None,
         }
     }
 }
