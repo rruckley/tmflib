9f88537e20c89cf91b08dee4b332f20db6577e55
Ryan Ruckley <rruckley@gmail.com>
2023-09-28 13:16:26 +1000

    use fn to link

--- examples/link_offers.rs
+++ examples/link_offers.rs
@@ -1,17 +1,14 @@
 //! Example to link Product Offers
 //! 
 
-use tmflib::tmf620::product_offering::{ProductOffering,ProductOfferingRelationship};
+use tmflib::tmf620::product_offering::ProductOffering;
 
 fn main() {
     let offer = ProductOffering::new(String::from("OriginalProduct"));
 
-    let mut offer_rel = ProductOfferingRelationship::from(offer);
-    offer_rel.relationship_type = Some(String::from("ParentChild"));
-    offer_rel.role = Some(String::from("parent"));
-
     let mut new_offer = ProductOffering::new(String::from("NewProduct"));
-    new_offer.product_offering_relationship.as_mut().unwrap().push(offer_rel);
+
+    new_offer.link_po(offer, "ParentChild", "Child");
 
     dbg!(new_offer);
 }
