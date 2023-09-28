b0f1a6165376efc92883224befd6621d425a6d89
Ryan Ruckley <rruckley@gmail.com>
2023-09-28 13:16:32 +1000

    new link fn

--- src/tmf620/product_offering.rs
+++ src/tmf620/product_offering.rs
@@ -205,6 +205,15 @@ impl ProductOffering {
             .push(ProductSpecificationRef::from(specification));
         self
     }
+
+    // Create a link between two ProductOfferings
+    pub fn link_po(&mut self, remote_po : ProductOffering, relationship_type : &str, role : &str) {
+        // Create a link from ourselves into remote_po using type and role prodived.
+        let mut offer_rel = ProductOfferingRelationship::from(remote_po);
+        offer_rel.relationship_type = Some(relationship_type.to_string());
+        offer_rel.role = Some(role.to_string());
+        self.product_offering_relationship.as_mut().unwrap().push(offer_rel);
+    }
 }
 
 #[cfg(test)]
