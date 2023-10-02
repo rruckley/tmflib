a3855f13a645761658f9f57855e274b84a2fee1a
Ryan Ruckley <rruckley@gmail.com>
2023-10-03 10:52:19 +1100

    char value spec structs

--- src/tmf620/product_specification.rs
+++ src/tmf620/product_specification.rs
@@ -9,6 +9,8 @@ use super::MOD_PATH;
 
 const SPEC_PATH: &str = "productSpecification";
 const SPEC_VERS: &str = "1.0";
+const CHAR_VALUE_MIN_CARD : u16 = 0;
+const CHAR_VALUE_MAX_CARD : u16 = 1;
 
 /// Product Specification Characteristic
 #[derive(Clone, Debug, Default, Deserialize, Serialize)]
@@ -39,8 +41,8 @@ impl ProductSpecificationCharacteristic {
             description: None,
             extensible: false,
             is_unique: false,
-            max_cardinality: 1,
-            min_cardinality: 0,
+            max_cardinality: CHAR_VALUE_MAX_CARD,
+            min_cardinality: CHAR_VALUE_MIN_CARD,
             name,
             regex: None,
             value_type: None,
@@ -158,16 +160,93 @@ impl From<ProductSpecification> for ProductSpecificationRef {
     }
 }
 
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct ProductSpecificationCharacteristicValue {
+    is_default: bool,
+    range_interval: Option<String>,
+    regex: Option<String>,
+    unit_of_measure: Option<String>,
+    value_from: Option<String>,
+    value_to: Option<String>,
+    value_type: Option<String>,
+    value_for: Option<String>,
+    value: String,
+}
+
+impl ProductSpecificationCharacteristicValue {
+    pub fn new(value : String) -> ProductSpecificationCharacteristicValue {
+        ProductSpecificationCharacteristicValue {
+            is_default: false,
+            range_interval: None,
+            regex: None,
+            unit_of_measure: None,
+            value_from: None,
+            value_to: None,
+            value_type: None,
+            value_for: None,
+            value,
+        }
+    }
+}
+
 /// Product Specification Characteristic Value Use
 #[derive(Clone, Debug, Deserialize, Serialize)]
-pub struct ProductSpecificationCharacteristicValueUse {}
+pub struct ProductSpecificationCharacteristicValueUse {
+    description: Option<String>,
+    max_cardinality: u16,
+    min_cardinality: u16,
+    name: String,
+    value_type: String,
+    valid_for: Option<String>,
+    product_spec_characteristic_value : Option<ProductSpecificationCharacteristicValue>,
+}
+
+impl ProductSpecificationCharacteristicValueUse {
+    pub fn new(name : String) -> ProductSpecificationCharacteristicValueUse {
+        ProductSpecificationCharacteristicValueUse { 
+            description: None, 
+            max_cardinality: 1, 
+            min_cardinality: 0, 
+            name, 
+            value_type: String::from("String"), 
+            valid_for: None,
+            product_spec_characteristic_value : None,
+        }
+    }
+}
 
 #[cfg(test)]
 mod test {
 
     use super::ProductSpecification;
+    use super::ProductSpecificationCharacteristicValueUse;
     use super::SPEC_VERS;
+    use super::CHAR_VALUE_MIN_CARD;
+    use super::CHAR_VALUE_MAX_CARD;
     const SPEC_NAME: &str = "MySpecification";
+    const VALUE_NAME: &str = "MyCharValueUse";
+
+    #[test]
+    fn test_char_value_use_new() {
+        let value_use = ProductSpecificationCharacteristicValueUse::new(VALUE_NAME.to_string());
+
+        assert_eq!(value_use.name, VALUE_NAME.to_string());
+    }
+
+    #[test]
+    fn test_char_value_use_new_card() {
+        let value_use = ProductSpecificationCharacteristicValueUse::new(VALUE_NAME.to_string());
+
+        assert_eq!(value_use.min_cardinality, CHAR_VALUE_MIN_CARD);
+        assert_eq!(value_use.max_cardinality, CHAR_VALUE_MAX_CARD);
+    }
+
+    #[test]
+    fn test_char_value_use_new_value() {
+        let value_use = ProductSpecificationCharacteristicValueUse::new(VALUE_NAME.to_string());
+
+        assert_eq!(value_use.value_type, String::from("String"));
+    }
 
     #[test]
     fn test_spec_new() {
