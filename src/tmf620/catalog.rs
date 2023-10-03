3ad9c829749f254a6ab0bb6f005530a4ecca9148
Ryan Ruckley <rruckley@gmail.com>
2023-10-03 17:55:24 +1100

    event trait

--- src/tmf620/catalog.rs
+++ src/tmf620/catalog.rs
@@ -3,6 +3,7 @@
 //!
 use crate::tmf620::category::CategoryRef;
 use crate::tmf620::party::RelatedParty;
+use crate::common::event::{Event,EventPayload};
 
 use chrono::naive::NaiveDateTime;
 use chrono::Utc;
@@ -17,23 +18,32 @@ const CAT_PATH: &str = "catalog";
 const CAT_VERS: &str = "1.0";
 
 /// Catalogue
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Clone,Debug, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Catalog {
     /// Non-optional fields
     id: String,
     href: String,
     /// Optional fields
+    #[serde(skip_serializing_if = "Option::is_none")]
     catalog_type: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     description: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     last_update: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     lifecycle_status: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     name: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     version: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     valid_for: Option<String>,
     /// Categories
+    #[serde(skip_serializing_if = "Option::is_none")]
     category: Option<Vec<CategoryRef>>,
     /// Related parties
+    #[serde(skip_serializing_if = "Option::is_none")]
     related_party: Option<Vec<RelatedParty>>,
 }
 
@@ -82,6 +92,50 @@ impl std::default::Default for Catalog {
         }
     }
 }
+
+impl EventPayload<Catalog,CatalogEventType> for Catalog {
+    fn generate_event(&self,event_type : CatalogEventType) -> crate::common::event::Event<Catalog,CatalogEventType> {       
+        let now = Utc::now();
+        let event_time = NaiveDateTime::from_timestamp_opt(now.timestamp(), 0).unwrap();
+        Event {
+            correlation_id: None,
+            description: None,
+            domain: None,
+            event_id: Uuid::new_v4().to_string(),
+            field_path: None,
+            href: Some(self.href.clone()),
+            id: Some(self.id.clone()),
+            title: self.name.clone(),
+            event_time: event_time.to_string(),
+            priority: None,
+            time_occurred: None,
+            event_type,
+            event: self.clone(),
+        }
+    }
+}
+
+#[derive(Debug,Deserialize,Serialize)]
+pub enum CatalogEventType {
+    CatalogCreateEvent,
+    CatalogDeleteEvent,
+    CatalogBatchEvent,
+}
+
+impl std::fmt::Display for CatalogEventType {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            CatalogEventType::CatalogCreateEvent => write!(f, "CatalogCreateEvent"),
+            CatalogEventType::CatalogDeleteEvent => write!(f, "CatalogDeleteEvent"),
+            CatalogEventType::CatalogBatchEvent => write!(f, "CatalogBatchEvent"),
+        }
+    }
+}
+
+// Notifications
+pub struct CatalogCreateEvent {}
+pub struct CatalogDeleteEvent {}
+pub struct CatalogBatchEvent {}
 
 #[cfg(test)]
 mod tests {
