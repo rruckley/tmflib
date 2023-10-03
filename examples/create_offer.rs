3a24f085c4f099b77bcb2ee22ec2063071ecb16c
Ryan Ruckley <rruckley@gmail.com>
2023-10-03 13:17:15 +1100

    expanded example.

--- examples/create_offer.rs
+++ examples/create_offer.rs
@@ -2,11 +2,24 @@
 //! 
 use tmflib::tmf620::product_offering::ProductOffering;
 use tmflib::tmf620::category::{Category,CategoryRef};
+use tmflib::tmf620::product_specification::{ProductSpecification,ProductSpecificationCharacteristic};
 
 fn main() {
+    let char1 = ProductSpecificationCharacteristic::new(String::from("OptionalChar"))
+    .description(String::from("This characteristic is optional."));
+    let char2 = ProductSpecificationCharacteristic::new(String::from("MandatoryChar"))
+    .cardinality(1,1)
+    .description(String::from("This is a mandatory characteristic."));
+
+
+let spec = ProductSpecification::new(String::from("MySpecification"))
+    .with_charateristic(char1)
+    .with_charateristic(char2);
+
     let category = Category::new(String::from("Template"));
     let offer = ProductOffering::new(String::from("ProductOffering"))
-        .with_category(CategoryRef::from(&category));
+        .with_category(CategoryRef::from(&category))
+        .with_specification(spec);
 
     dbg!(&offer);
 }
