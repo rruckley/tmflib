88979088b35131a054d48628d145a66cee3414c9
Ryan Ruckley <rruckley@gmail.com>
2023-10-09 15:20:50 +1100

    use traits

--- src/tmf637/product.rs
+++ src/tmf637/product.rs
@@ -1,7 +1,9 @@
 //! Product Module
 //!
 use serde::{Deserialize, Serialize};
-use uuid::Uuid;
+
+use crate::CreateTMF;
+use crate::HasId;
 
 use super::LIB_PATH;
 use super::MOD_PATH;
@@ -23,23 +25,37 @@ enum ProductStatusType {
 
 #[derive(Debug, Default, Deserialize, Serialize)]
 pub struct Product {
-    id: String,
-    href: String,
+    id: Option<String>,
+    href: Option<String>,
     description: Option<String>,
     name: String,
     status: ProductStatusType,
 }
 
+impl HasId for Product {
+    fn generate_href(&mut self) {
+        let href = format!("/{}/{}/{}/{}", LIB_PATH, MOD_PATH, PROD_PATH, self.get_id());
+        self.href = Some(href);    
+    }
+    fn generate_id(&mut self) {
+        let id = self.get_uuid();
+        self.id = Some(id);
+        self.generate_href();
+    }
+    fn get_href(&mut self) -> String {
+        self.href.as_ref().unwrap().clone()    
+    }
+    fn get_id(&mut self) -> String {
+        self.id.as_ref().unwrap().clone()    
+    }
+}
+impl CreateTMF<Product> for Product {}
+
 impl Product {
     pub fn new(name: String) -> Product {
-        let id = Uuid::new_v4().to_string();
-        let href = format!("/{}/{}/{}/{}", LIB_PATH, MOD_PATH, PROD_PATH, id);
-        Product {
-            id,
-            href,
-            description: None,
-            name,
-            status: ProductStatusType::Created,
-        }
+        let mut product = Product::create();
+        product.status = ProductStatusType::Created;
+        product.name = name;
+        product
     }
 }
