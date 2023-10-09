31f51293d1f73bd988097edb35a17c7ded9b2d07
Ryan Ruckley <rruckley@gmail.com>
2023-10-09 11:23:48 +1100

    create fn

--- src/tmf700/shipping_order.rs
+++ src/tmf700/shipping_order.rs
@@ -18,6 +18,14 @@ pub struct ShippingOrder {
     pub href: Option<String>,
 }
 
+impl ShippingOrder {
+    pub fn new() -> ShippingOrder {
+        let mut so = ShippingOrder::default();
+        so.generate_id();
+        so
+    }
+}
+
 impl HasId for ShippingOrder {
     fn generate_href(&mut self) {
         let href = format!("/{}/{}/{}/{}",LIB_PATH,MOD_PATH,SHIP_PATH,self.id.as_ref().unwrap().clone()); 
@@ -39,5 +47,34 @@ impl HasId for ShippingOrder {
             self.generate_id();
         }  
         self.id.as_ref().unwrap().clone()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::ShippingOrder;
+    use super::HasId;
+    #[test]
+    fn shipping_order_create_id() {
+        // Generate shipping order, test id
+        let so = ShippingOrder::new();
+
+        assert_eq!(so.id.is_some(),true);
+    }
+
+    #[test]
+    fn shipping_order_create_href() {
+        let so = ShippingOrder::new();
+
+        assert_eq!(so.href.is_some(), true);
+    }
+
+    #[test]
+    fn shipping_order_create_href_matches_id() {
+        let mut so = ShippingOrder::new();
+        let id = so.get_id();
+        let href = so.get_href();
+
+        assert!(href.contains(&id));
     }
 }
