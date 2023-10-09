84948186515f840b21bdeb5ee28844d4523af77a
Ryan Ruckley <rruckley@gmail.com>
2023-10-09 11:24:43 +1100

    impl trait

--- src/tmf622/product_order.rs
+++ src/tmf622/product_order.rs
@@ -3,6 +3,8 @@
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
+use crate::HasId;
+
 // URL Path components
 use super::LIB_PATH;
 use super::MOD_PATH;
@@ -11,14 +13,38 @@ const PO_PATH: &str = "order";
 
 #[derive(Debug, Default, Deserialize, Serialize)]
 pub struct ProductOrder {
-    id: String,
-    href: String,
+    id: Option<String>,
+    href: Option<String>,
 }
 
 impl ProductOrder {
     pub fn new() -> ProductOrder {
-        let id = Uuid::new_v4().to_string();
-        let href = format!("/{}/{}/{}/{}", LIB_PATH, MOD_PATH, PO_PATH, id);
-        ProductOrder { id, href }
+        let mut po = ProductOrder::default();
+        po.generate_id();
+        po
+    }
+}
+
+impl HasId for ProductOrder {
+    fn generate_href(&mut self) {
+        let id = self.get_id();
+        let href = format!("/{}/{}/{}/{}", LIB_PATH, MOD_PATH, PO_PATH, id);    
+        self.href = Some(href);
+    }
+    fn generate_id(&mut self) {
+        let id = Uuid::new_v4().simple().to_string();
+        self.id = Some(id);    
+    }
+    fn get_href(&mut self) -> String {
+        if self.href.is_none() {
+            self.generate_href();
+        } 
+        self.href.as_ref().unwrap().clone()   
+    }
+    fn get_id(&mut self) -> String {
+        if self.id.is_none() {
+            self.generate_id();
+        }    
+        self.id.as_ref().unwrap().clone()
     }
 }
