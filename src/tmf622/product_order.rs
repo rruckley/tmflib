dfbf4b11daa29869c81ae8e7a30695328ac8ce38
Ryan Ruckley <rruckley@gmail.com>
2023-10-09 15:27:20 +1100

    handle order_items

--- src/tmf622/product_order.rs
+++ src/tmf622/product_order.rs
@@ -4,11 +4,14 @@ use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
 use super::HasId;
+use crate::common::related_party::RelatedParty;
 use crate::{CreateTMFWithTime,HasLastUpdate};
 
 // URL Path components
 use super::LIB_PATH;
 use super::MOD_PATH;
+
+use super::product_order_item::ProductOrderItem;
 
 const PO_PATH: &str = "order";
 
@@ -17,6 +20,8 @@ pub struct ProductOrder {
     id: Option<String>,
     href: Option<String>,
     order_date: Option<String>,
+    product_order_item: Vec<ProductOrderItem>,
+    related_party: Vec<RelatedParty>,
 }
 
 impl HasLastUpdate for ProductOrder {
@@ -31,6 +36,14 @@ impl ProductOrder {
     pub fn new() -> ProductOrder {
         ProductOrder::create_with_time()
     }
+
+    pub fn add_order_item(&mut self, order_item : ProductOrderItem) {
+        self.product_order_item.push(order_item);
+    }
+
+    pub fn add_party(&mut self, party: RelatedParty) {
+        self.related_party.push(party);
+    }
 }
 
 impl HasId for ProductOrder {
@@ -41,7 +54,8 @@ impl HasId for ProductOrder {
     }
     fn generate_id(&mut self) {
         let id = Uuid::new_v4().simple().to_string();
-        self.id = Some(id);    
+        self.id = Some(id);
+        self.generate_href(); 
     }
     fn get_href(&mut self) -> String {
         if self.href.is_none() {
