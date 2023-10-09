ca4aef6ee6d1d425376fba99cc46b9eff6f8066a
Ryan Ruckley <rruckley@gmail.com>
2023-10-09 16:37:32 +1100

    update example

--- examples/create_product_order.rs
+++ examples/create_product_order.rs
@@ -4,13 +4,15 @@ use tmflib::tmf622::product_order::ProductOrder;
 use tmflib::tmf622::product_order_item::ProductOrderItem;
 use tmflib::tmf620::product_offering::ProductOffering;
 use tmflib::tmf629::customer::Customer;
+use tmflib::tmf632::individual::Individual;
 
 fn main() {
     let offer = ProductOffering::new(String::from("Sample Offering"));
     let customer = Customer::new(String::from("Sample Customer"));
+    let person = Individual::new("Ryan Ruckley".to_string());
     let mut order = ProductOrder::new();
     order.add_order_item(ProductOrderItem::from(offer));
     order.add_party(RelatedParty::from(&customer));
-
+    order.add_party(RelatedParty::from(&person));
     dbg!(order);
 }
