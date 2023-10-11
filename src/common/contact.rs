af2c43621e204c26a0320b5b8f5cd1905d4a0769
Ryan Ruckley <rruckley@gmail.com>
2023-10-11 17:58:01 +1100

    moved to common

---
+++ src/common/contact.rs
@@ -0,0 +1,50 @@
+//! Contact Module
+//!
+use serde::{Deserialize, Serialize};
+use std::hash::Hash;
+
+
+#[derive(Clone, Debug, Default, Deserialize, Hash, Serialize)]
+pub struct MediumCharacteristic {
+    email_address : Option<String>,
+}
+
+/// Contact Medium
+#[derive(Clone, Default, Debug, Deserialize, Hash, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ContactMedium {
+    characteristic: Option<MediumCharacteristic>,
+    medium_type: Option<String>,
+    preferred: bool,
+}
+
+impl ContactMedium {
+    pub fn new() -> ContactMedium {
+        ContactMedium {
+            preferred: false,
+            medium_type: None,
+            characteristic: None,
+        }
+    }
+
+    /// Generate a new contact medium representing email address
+    pub fn email(email: &str) -> ContactMedium {
+        let char = MediumCharacteristic {
+            email_address : Some(email.to_string()),
+        };
+        let medium = ContactMedium {
+            preferred : false,
+            medium_type : Some(String::from("email")),
+            characteristic : Some(char),
+        };
+        medium
+    }
+}
+
+/// Contact Characteristic
+#[derive(Clone, Default, Debug, Deserialize, Hash, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ContactCharacteristic {
+    email_address: Option<String>,
+    phone_number: Option<String>,
+}
