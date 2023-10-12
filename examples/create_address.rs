927ec40438dae343611891c82eca82edc8cbe2e8
Ryan Ruckley <rruckley@gmail.com>
2023-10-12 14:17:45 +1100

    expand

--- examples/create_address.rs
+++ examples/create_address.rs
@@ -4,7 +4,12 @@
 use tmflib::tmf673::geographic_address::GeographicAddress;
 
 fn main() {
-    let address = GeographicAddress::new(String::from("Site 1"));
+    let address = GeographicAddress::new(String::from("Site 1"))
+        .number("5")
+        .street("Roseland")
+        .street_type("Avenue")
+        .suburb("Norhtshore")
+        .state("NSW");
 
     dbg!(address);
 }
