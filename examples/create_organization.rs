95214cefbb528a97b64af50f9afa0007a9fc1039
Ryan Ruckley <rruckley@gmail.com>
2023-10-16 17:48:15 +1100

    use two methods to create

--- examples/create_organization.rs
+++ examples/create_organization.rs
@@ -3,7 +3,14 @@
 use tmflib::tmf632::organization::Organization;
 
 fn main() {
-    let organization = Organization::new(String::from("MyOrganization"));
 
-    dbg!(organization);
+    // Two methods to create an org object
+
+    // 1) use native constructor new()
+    let org1 = Organization::new(String::from("My Organization"));
+    // 2) Use conversion from a String.
+    let org2 : Organization = String::from("My Organisation").into();
+
+    dbg!(org1);
+    dbg!(org2);
 }
