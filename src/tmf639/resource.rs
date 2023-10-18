9a22cf94bcd66f94cd8db6e13ca2fe079f48110a
Ryan Ruckley <rruckley@gmail.com>
2023-10-19 08:48:23 +1100

    expand resource

--- src/tmf639/resource.rs
+++ src/tmf639/resource.rs
@@ -4,19 +4,81 @@ use serde::{Deserialize,Serialize};
 use uuid::Uuid;
 
 use crate::common::related_party::RelatedParty;
+use crate::common::attachment::AttachmentRefOrValue;
 use super::MOD_PATH;
+use super::characteristic::Characteristic;
 use crate::{HasId, CreateTMF, LIB_PATH};
 
-const RESOURCE_PATH : &str  = "resource";
+const RESOURCE_PATH : &str = "resource";
+const RESOURCE_VERS : &str = "1.0";
+
+/// Resource Usage Status
+#[derive(Clone, Debug, Default, Deserialize, Serialize)]
+pub enum ResourceUsageStateType {
+    /// Idle
+    #[default]
+    Idle,
+    /// Active
+    Active,
+    /// Busy
+    Busy,
+}
+
+/// Adminsitrative configuration of resource
+#[derive(Clone, Debug, Default, Deserialize, Serialize)]
+pub enum ResourceAdministrativeStateType {
+    /// Resource has been locked
+    Locked,
+    /// Resource has been unlocked
+    #[default]
+    Unlocked,
+    /// Resource has been shutdown
+    Shutdown,
+}
+
+/// Operational Status of resource
+#[derive(Clone, Debug, Default, Deserialize, Serialize)]
+pub enum ResourceOperationalStateType {
+    /// Resource is enabled
+    #[default]
+    Enable,
+    /// Resource is disabled
+    Disable,
+}
+
+/// Resource Status
+#[derive(Clone, Debug, Default, Deserialize, Serialize)]
+pub enum ResourceStatusType {
+    /// Standby
+    Standby,
+    /// Resource is in alarm
+    Alarm,
+    /// Resource is available
+    #[default]
+    Available,
+    /// Resource has been reserved
+    Reserved,
+    /// Unknown status
+    Unknown,
+    /// Resource has been suspended
+    Suspended,
+}
 
 /// TMF Resource 
 #[derive(Clone, Debug, Default, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Resource {
+    administrative_state: ResourceAdministrativeStateType,
+    attachment: Vec<AttachmentRefOrValue>,
     id: Option<String>,
     href: Option<String>,
     name: String,
+    operational_state: ResourceOperationalStateType,
+    resource_characteristic: Vec<Characteristic>,
+    resource_status: ResourceStatusType,
+    resource_version: Option<String>,
     related_party: Vec<RelatedParty>,
+    usage_state: ResourceUsageStateType,
 }
 
 impl CreateTMF<Resource> for Resource {}
@@ -26,6 +88,7 @@ impl Resource {
     pub fn new(name : &str) -> Resource {
         let mut resource = Resource::create();
         resource.name = name.to_owned();
+        resource.resource_version = Some(RESOURCE_VERS.to_owned());
         resource    
     }
 
