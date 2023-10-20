b5016bbd9773f1deb4d11d575a1f0abb57d4899a
Ryan Ruckley <rruckley@gmail.com>
2023-10-20 16:37:13 +1100

    add note

--- src/tmf663/cart_item.rs
+++ src/tmf663/cart_item.rs
@@ -15,6 +15,12 @@ pub struct CartItem {
     product_offering: Option<ProductOfferingRef>,
 }
 
+impl CartItem {
+    pub fn add_note(&mut self, note : Note) {
+        self.note.push(note);
+    }
+}
+
 impl From<ProductOfferingRef> for CartItem {
     fn from(value: ProductOfferingRef) -> Self {
         let id = Uuid::new_v4().simple().to_string();
