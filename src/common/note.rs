3376db6f0ea818b72609cc004db4235ec853b969
Ryan Ruckley <rruckley@gmail.com>
2023-10-20 16:36:50 +1100

    create from str tests

--- src/common/note.rs
+++ src/common/note.rs
@@ -1,6 +1,10 @@
 //! Note Module
 
 use serde::{Deserialize, Serialize};
+use chrono::naive::NaiveDateTime;
+use chrono::Utc;
+use uuid::Uuid;
+use std::convert::From;
 
 /// Notes object for journaling against many TMF objects
 #[derive(Clone, Debug, Deserialize, Serialize)]
@@ -10,4 +14,50 @@ pub struct Note {
     author: Option<String>,
     date: Option<String>,
     text: Option<String>,
+}
+
+impl Note {
+    /// Create a new note, without author
+    pub fn new(text : String) -> Note {
+        let id = Uuid::new_v4().simple().to_string();
+        let now = Utc::now();
+        let time = NaiveDateTime::from_timestamp_opt(now.timestamp(), 0).unwrap();
+        Note { 
+            id, 
+            author: None, 
+            date: Some(time.to_string()), 
+            text : Some(text),
+        }
+    }
+    /// Set author for note with builder pattern
+    pub fn author(mut self, author : &str) -> Note {
+        self.author = Some(author.to_string());
+        self
+    }
+}
+
+impl From<&str> for Note {
+    fn from(value: &str) -> Self {
+        Note::new(value.to_string())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::Note;
+    #[test]
+    fn test_note_create_str() {
+        let note = Note::from("StringSlice");
+
+        assert_eq!(note.text.is_some(),true);
+        assert_eq!(note.text.unwrap(),"StringSlice");
+    }
+    #[test]
+    fn test_note_create_author() {
+        let note = Note::from("StringSlice")
+        .author("AnAuthor");
+
+        assert_eq!(note.author.is_some(),true);
+        assert_eq!(note.author.unwrap(),"AnAuthor".to_string());
+    }
 }
