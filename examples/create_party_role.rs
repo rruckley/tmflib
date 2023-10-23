336c06ae5faec5b44d4a86b4828ae43fea3afc6c
Ryan Ruckley <rruckley@gmail.com>
2023-10-23 11:22:05 +1100

    new example

---
+++ examples/create_party_role.rs
@@ -0,0 +1,23 @@
+//! Create Party Role Example
+//! # Description
+//! This example shows creating a party role "Account Manager", connecting
+//! it to an individual and associating it with a customer.
+//! This role could then be linked into an order or quote as a related party.
+
+
+use tmflib::tmf669::party_role::PartyRole;
+use tmflib::tmf629::customer::Customer;
+use tmflib::tmf632::individual::Individual;
+use tmflib::tmf632::organization::Organization;
+use tmflib::common::related_party::RelatedParty;
+
+fn main() {
+    let individual = Individual::new("John Smith");
+    let organisation = Organization::new("A Customer".to_string());
+    let customer = Customer::new(organisation);
+    let mut role = PartyRole::new("Account Manager")
+        .engaged_party(RelatedParty::from(&individual));
+    role.add_party(RelatedParty::from(&customer));
+
+    dbg!(role);
+}
