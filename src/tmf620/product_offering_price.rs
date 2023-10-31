45f59f4171d4b48bfed1d1480e9545ffdda4ea67
Ryan Ruckley <rruckley@gmail.com>
2023-11-01 08:23:41 +1100

    expand payload

--- src/tmf620/product_offering_price.rs
+++ src/tmf620/product_offering_price.rs
@@ -3,38 +3,117 @@
 use serde::{Deserialize,Serialize};
 
 use super::MOD_PATH;
-use crate::{HasId,CreateTMF, LIB_PATH};
+use crate::{HasId,CreateTMF, LIB_PATH, TimePeriod};
 const PRICE_PATH : &str = "productOfferingPrice ";
 const PRICE_VERS : &str = "1.0";
 
+/// Constraints
+#[derive(Clone, Default, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ConstraintRef {
+    /// Unique Id
+    id: String,
+    /// HTTP Reference
+    href: String,
+    /// Name
+    name: String,
+    /// Version
+    #[serde(skip_serializing_if = "Option::is_none")]
+    version: Option<String>,
+}
+
+#[derive(Clone, Default, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+
+/// Tax Amount in local currency
+pub struct TaxAmount {
+    unit: String,
+    value: u32,
+}
+
+/// Tax Details
+#[derive(Clone, Default, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct TaxItem {
+    tax_category: String,
+    tax_rate: f32,
+    tax_amount: TaxAmount,
+}
+
 /// Product Offering Price Reference
 #[derive(Clone, Default, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
 pub struct ProductOfferingPriceRef {
-    id: String,
-    href: String,
-    name: String,
+    /// Id
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub id: Option<String>,
+    /// HTTP Reference
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub href: Option<String>,
+    /// Name
+    pub name: String,
 }
 
 impl From<ProductOfferingPrice> for ProductOfferingPriceRef {
     fn from(pop : ProductOfferingPrice) -> ProductOfferingPriceRef {
-        ProductOfferingPriceRef { id: pop.id.unwrap().clone(), href: pop.href.unwrap().clone(), name: pop.name.clone() }
+        ProductOfferingPriceRef { 
+            id: pop.id.clone(), 
+            href: pop.href.clone(), 
+            name: pop.name.clone(),
+        ..Default::default()
+        }
     }
+}
+
+/// Price structure
+#[derive(Clone, Default, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Price {
+    unit : String,
+    amount : f32,
 }
 
 /// Pricing linked to a Product Offering
 #[derive(Clone, Default, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
 pub struct ProductOfferingPrice {
-    id: Option<String>,
-    href: Option<String>,
-    name: String,
-    version: String,
+    /// Unique Id
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub id: Option<String>,
+    /// HTML Reference
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub href: Option<String>,
+    /// Name
+    pub name: String,
+    /// Versoin
+    pub version: Option<String>,
+    constraint: Option<Vec<ConstraintRef>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    description: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    is_bundle: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    last_update: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    lifecycle_status: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    percentage: Option<f32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    price_type: Option<String>,
+    recurring_charge_period_length: Option<u16>,
+    recurring_charge_period_type: String,
+    price: Price,
+    unit_of_measure: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    valid_for: Option<TimePeriod>,
+    tax: Vec<TaxItem>,
 }
 
 impl ProductOfferingPrice {
     /// Create a new Price Offering Price object
     pub fn new(name :  String) -> ProductOfferingPrice {
         let mut pop = ProductOfferingPrice::create();
-        pop.version = PRICE_VERS.to_string();
+        pop.version = Some(PRICE_VERS.to_string());
         pop.name = name;
         pop
     }
