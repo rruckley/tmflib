b4532329399305a5b8270b147390679668e471fd
Ryan Ruckley <rruckley@gmail.com>
2023-11-14 15:43:51 +1100

    pull in consts

--- src/tmf653/service_test.rs
+++ src/tmf653/service_test.rs
@@ -2,6 +2,11 @@
 
 use serde::{Deserialize,Serialize};
 
+use crate::{HasId,CreateTMF,TimePeriod,LIB_PATH};
+use super::MOD_PATH;
+
+const TEST_PATH : &str = "serviceTest";
+
 /// Test execution status
 #[derive(Clone,Debug,Default,Deserialize,Serialize)]
 pub enum ExecutionStateType {
@@ -33,4 +38,37 @@ pub struct ServiceTest {
     name: Option<String>,
     start_date_time: Option<String>,
     state: Option<ExecutionStateType>,
+    valid_for: Option<TimePeriod>,
 }
+
+impl ServiceTest {
+    /// Create new ServiceTest
+    pub fn new(name : &str) -> ServiceTest {
+        let mut st = ServiceTest::create();
+        st.name = Some(name.to_owned());
+        st
+    }
+}
+
+impl HasId for ServiceTest {
+    fn generate_href(&mut self) {
+        let href = format!("/{}/{}/{}/{}",LIB_PATH,MOD_PATH,TEST_PATH,self.get_id());
+        self.href = Some(href);        
+    }
+    fn generate_id(&mut self) {
+        let id = ServiceTest::get_uuid();
+        self.id = Some(id);  
+        self.generate_href();
+    }
+    fn get_class() -> String {
+        String::from("service_test")    
+    }
+    fn get_href(&self) -> String {
+        self.href.as_ref().unwrap().clone()
+    }
+    fn get_id(&self) -> String {
+        self.id.as_ref().unwrap().clone()    
+    }
+}
+
+impl CreateTMF<ServiceTest> for ServiceTest {}
