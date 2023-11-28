e00acb06ad9e849d95060449aa037c29ab003ad3
Ryan Ruckley <rruckley@gmail.com>
2023-11-28 13:33:38 +1100

    demo setting cost

--- examples/create_cost.rs
+++ examples/create_cost.rs
@@ -1,10 +1,11 @@
 //! Create Cost Example
 
-use tmflib::tmf7xx::cost::Cost;
+use tmflib::tmf7xx::cost::{CostEntry,Cost};
 
 fn main() {
     let mut cost = Cost::new("MyCost");
-    let child_cost = Cost::new("ChildCost");
+    let child_cost = Cost::new("ChildCost")
+        .cost(CostEntry{unit: "Dollars".to_string(),amount: 10.0});
 
     cost.add_child(child_cost);
 
