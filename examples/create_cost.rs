6ae3d0bbb72533d77f85503f2f3a4702a832e3a3
Ryan Ruckley <rruckley@gmail.com>
2023-11-28 17:03:38 +1100

    expanded example.

--- examples/create_cost.rs
+++ examples/create_cost.rs
@@ -4,11 +4,14 @@ use tmflib::tmf7xx::cost::{CostEntry,Cost};
 
 fn main() {
     let mut cost = Cost::new("ParentCost")
-        .cost(CostEntry{unit: "Dollars".to_string(),amount: 37.5});
-    let child_cost = Cost::new("ChildCost")
-        .cost(CostEntry{unit: "Dollars".to_string(),amount: 10.0});
+        .cost(CostEntry{unit: "Dollars".to_string(),amount: 237.5});
+    let child_cost_1 = Cost::new("ChildCost")
+        .cost(CostEntry{unit: "Dollars".to_string(),amount: 101.0});
+    let child_cost_2 = Cost::new("Second Child")
+        .cost(CostEntry{unit: "Dollars".to_string(),amount : 125.99});
 
-    cost.add_child(child_cost);
+    cost.add_child(child_cost_1);
+    cost.add_child(child_cost_2);
 
     dbg!(&cost);
 
