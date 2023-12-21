25403da82549df2ba102b82feeb75993de3b56d6
Ryan Ruckley <rruckley@gmail.com>
2023-12-21 17:27:43 +1100

    Support calendar for GeoSite

--- src/tmf674/geographic_site.rs
+++ src/tmf674/geographic_site.rs
@@ -9,6 +9,7 @@ use crate::tmf673::geographic_address::GeographicAddress;
 use crate::LIB_PATH;
 use super::MOD_PATH;
 const GEO_PATH: &str = "site";
+const DEFAULT_TZ : &str = "AEST";
 
 /// Refernce to a place
 /// # Uses
@@ -43,6 +44,39 @@ pub struct GeographicSiteRelationship {
     valid_for: TimePeriod,
 }
 
+/// Definition of start and finish hours
+#[derive(Clone, Debug, Default, Deserialize, Serialize)]
+pub struct HourPeriod {
+    start_hour : String,
+    end_hour : String,
+}
+
+/// Calendar entry defining periodic status for site, e.g. opening hours
+#[derive(Clone, Debug, Default, Deserialize, Serialize)]
+pub struct CalendarPeriod {
+    day : Option<String>,
+    status : Option<String>,
+    time_zone : Option<String>,
+    hour_period : Option<Vec<HourPeriod>>,
+}
+
+impl CalendarPeriod {
+    /// Generate standard business hours calendar
+    pub fn business_hours() -> CalendarPeriod {
+    CalendarPeriod {
+        day : Some("weekdays".to_string()),
+        status : Some("open".to_string()),
+        time_zone : Some(DEFAULT_TZ.to_string()),
+        hour_period : Some(
+            vec![HourPeriod{
+                start_hour : "09:00 am".to_string(),
+                end_hour : "05:00 pm".to_string(),
+            }
+            ]
+        )
+        }
+    }
+}
 /// Geographic Site
 #[derive(Clone, Debug, Default, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
@@ -69,6 +103,8 @@ pub struct GeographicSite {
     /// Relationships to other sides, e.g. floor , building,tenant
     #[serde(skip_serializing_if = "Option::is_none")]
     pub site_relationship: Option<Vec<GeographicSiteRelationship>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    calendar : Option<Vec<CalendarPeriod>>,
 }
 
 impl GeographicSite {
@@ -76,6 +112,7 @@ impl GeographicSite {
     pub fn new(name : String) -> GeographicSite {
         let mut site = GeographicSite::create();
         site.name = name;
+        site.calendar = Some(vec![]);
         site
     }
     /// Set the place on this Site
@@ -87,6 +124,12 @@ impl GeographicSite {
     /// Set the code for this site
     pub fn code(mut self, code : String) -> GeographicSite {
         self.code = Some(code);
+        self
+    }
+
+    /// Set the calendar for this site
+    pub fn calendar(mut self, calendar : CalendarPeriod) -> GeographicSite {
+        self.calendar.as_mut().unwrap().push(calendar);
         self
     }
 }
