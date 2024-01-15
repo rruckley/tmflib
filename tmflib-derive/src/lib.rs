fc089eae732b09f7bdd61dce68f2f6c121eccee4
Ryan Ruckley <rruckley@gmail.com>
2024-01-15 17:38:09 +1100

    Implement derive functions

--- tmflib-derive/src/lib.rs
+++ tmflib-derive/src/lib.rs
@@ -51,3 +51,50 @@ pub fn hasid_derive(input: TokenStream) -> TokenStream {
     out.into()
 }
 
+#[proc_macro_derive(HasLastUpdate)]
+pub fn haslastupdate_derive(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let fields = match input.data {
+        Data::Struct(s) => {
+            s.fields
+                .into_iter()
+                .map(|f| f.ident.unwrap().to_string()).collect::<Vec<_>>()
+            },
+        _ => panic!("HasId only supports Struct"),
+    };
+    let _last_update = fields.iter().find(|s| *s == "last_update").expect("No last_update field present");
+    let name = input.ident;
+    let out = quote! {
+        impl HasLastUpdate for #name {
+            fn set_last_update(&mut self, time : impl Into<String>) {
+                self.last_update = time.into();
+            }
+        }
+    
+    };
+    out.into()
+}
+
+#[proc_macro_derive(HasName)]
+pub fn hasname_derive(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let fields = match input.data {
+        Data::Struct(s) => {
+            s.fields
+                .into_iter()
+                .map(|f| f.ident.unwrap().to_string()).collect::<Vec<_>>()
+            },
+        _ => panic!("HasId only supports Struct"),
+    };
+    let name = input.ident;
+    // Ensure id field is present
+    let _name = fields.iter().find(|s| *s == "name").expect("No name field present");
+    let out = quote! {
+        impl HasId for #name {
+            fn get_name(&self) -> String {
+                self.name.clone().unwrap_or("NoName".to_string())
+            }
+        }
+    };
+    out.into()
+}
