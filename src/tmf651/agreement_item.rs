cb85abf370b4975f52a4e7c06051e73f3ef2e23d
Ryan Ruckley <rruckley@gmail.com>
2024-01-19 08:58:44 +1100

    add agreement_item

---
+++ src/tmf651/agreement_item.rs
@@ -0,0 +1,15 @@
+//! Agreement Item Module
+
+
+/// Agreement Item
+pub struct AgreementItem {
+
+}
+
+/// Agreement Item Reference
+pub struct AgreementItemRef {
+    id: String,
+    href: String,
+    agreementItemId: String,
+    name : String,
+}
