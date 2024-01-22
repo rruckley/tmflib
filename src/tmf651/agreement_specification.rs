89112e665d52ff13e847fc7b9eb4fe460db74706
Ryan Ruckley <rruckley@gmail.com>
2024-01-22 11:44:03 +1100

    optional serialize

--- src/tmf651/agreement_specification.rs
+++ src/tmf651/agreement_specification.rs
@@ -10,17 +10,27 @@ const CLASS_PATH : &str = "specification";
 /// Agreement Specification
 #[derive(Clone,Debug,Default,Deserialize,HasId,HasName,HasLastUpdate,Serialize)]
 pub struct AgreementSpecification {
+    #[serde(skip_serializing_if = "Option::is_none")]
     description: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     href: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     id: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     is_bundle: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     last_update: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     lifecycle_status: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     name: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     valid_for: Option<TimePeriod>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     version: Option<String>,
 }
 
+/// Reference to external specification
 #[derive(Clone,Default,Debug, Deserialize, Serialize)]
 pub struct AgreementSpecificationRef {
     description : String,
