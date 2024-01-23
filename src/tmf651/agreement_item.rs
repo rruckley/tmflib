e1248f17bd347d95233e0995f3ba017fb6bf3c88
Ryan Ruckley <rruckley@gmail.com>
2024-01-23 11:17:06 +1100

    Add std derives

--- src/tmf651/agreement_item.rs
+++ src/tmf651/agreement_item.rs
@@ -1,12 +1,24 @@
 //! Agreement Item Module
-
+use serde::{Deserialize,Serialize};
+use crate::{HasValidity,TimePeriod};
+use tmflib_derive::HasValidity;
 
 /// Agreement Item
+#[derive(Clone,Default,Debug, Deserialize, Serialize)]
 pub struct AgreementItem {
+    term_or_condition: Option<Vec<AgreementTermOrCondition>>,
+}
 
+/// Agreement Item terms and conditions
+#[derive(Clone,Default,Debug, Deserialize, HasValidity, Serialize)]
+pub struct AgreementTermOrCondition {
+    description: String,
+    id: String,
+    valid_for: Option<TimePeriod>,
 }
 
 /// Agreement Item Reference
+#[derive(Clone,Default,Debug, Deserialize, Serialize)]
 pub struct AgreementItemRef {
     id: String,
     href: String,
