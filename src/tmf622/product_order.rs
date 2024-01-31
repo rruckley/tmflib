05ae4c876213861aac3f482c62ec3ccaae26d4d6
Ryan Ruckley <rruckley@gmail.com>
2024-01-31 18:21:31 +1100

    missing fields

--- src/tmf622/product_order.rs
+++ src/tmf622/product_order.rs
@@ -6,7 +6,9 @@ use super::HasId;
 use tmflib_derive::HasId;
 use crate::tmf641::service_order::ServiceOrder;
 use crate::common::related_party::RelatedParty;
-use crate::{CreateTMF, CreateTMFWithTime,HasLastUpdate};
+use crate::common::note::Note;
+use crate::tmf651::agreement::AgreementRef;
+use crate::{CreateTMF, CreateTMFWithTime,HasLastUpdate,DateTime};
 
 // URL Path components
 use super::LIB_PATH;
@@ -24,10 +26,43 @@ pub struct ProductOrder {
     id: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
     href: Option<String>,
+    /// Cancellation Date
     #[serde(skip_serializing_if = "Option::is_none")]
-    order_date: Option<String>,
-    product_order_item: Vec<ProductOrderItem>,
-    related_party: Vec<RelatedParty>,
+    pub cancellation_date: Option<DateTime>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    /// Cancellation Reason
+    pub cancellation_reason: Option<String>,
+    /// Category
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub category : Option<String>,
+    /// Completion Date
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub completion_date: Option<DateTime>,
+    /// Description
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub description : Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    /// Expected Completion Date
+    pub expected_completion_date : Option<DateTime>,
+    /// External Id
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub external_id : Option<String>,
+    /// Order Date
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub order_date: Option<String>,
+    /// Product Order Items
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub product_order_item: Option<Vec<ProductOrderItem>>,
+    /// Related Parties
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub related_party: Option<Vec<RelatedParty>>,
+    // Referenced objects
+    /// Notes
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub note : Option<Vec<Note>>,
+    /// Agreements
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub agreement : Option<Vec<AgreementRef>>,
 }
 
 impl HasLastUpdate for ProductOrder {
@@ -46,7 +81,7 @@ impl ProductOrder {
 
     /// Add an ProductOrderItem into the ProductOrder
     pub fn add_order_item(&mut self, order_item : ProductOrderItem) {
-        self.product_order_item.push(order_item);
+        self.product_order_item.as_mut().unwrap().push(order_item);
     }
 
     /// Add a RelatedParty into the ProductOrder
@@ -64,7 +99,7 @@ impl ProductOrder {
     /// dbg!(order);
     /// ```
     pub fn add_party(&mut self, party: RelatedParty) {
-        self.related_party.push(party);
+        self.related_party.as_mut().unwrap().push(party);
     }
 }
 
@@ -72,6 +107,14 @@ impl From<ServiceOrder> for ProductOrder {
     fn from(value: ServiceOrder) -> Self {
         let mut po = ProductOrder::new();
         po.order_date = value.order_date.clone();
-        po    
+        po.description = value.description.clone();
+        po.order_date = value.order_date.clone();
+        po.category = value.category.clone();
+        po.cancellation_date = value.cancellation_date.clone();
+        po.note = value.note.clone();
+        po.expected_completion_date = value.expected_completion_date.clone();
+        po.external_id = value.external_id.clone();
+        po.related_party = value.related_party.clone();
+        po  
     }
 }
