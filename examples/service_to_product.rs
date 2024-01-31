803b9edff0bf51c719ece1ba8385f3d169a6d6ef
Ryan Ruckley <rruckley@gmail.com>
2024-01-31 18:21:06 +1100

    better example

--- examples/service_to_product.rs
+++ examples/service_to_product.rs
@@ -3,6 +3,11 @@
 use tmflib::tmf633::service_specification::ServiceSpecification;
 use tmflib::tmf633::characteristic_specification::CharacteristicSpecification;
 use tmflib::tmf620::product_specification::ProductSpecification;
+use tmflib::tmf641::service_order::ServiceOrder;
+use tmflib::tmf622::product_order::ProductOrder;
+use tmflib::tmf632::individual::Individual;
+use tmflib::common::note::Note;
+use tmflib::common::related_party::RelatedParty;
 
 fn main() {
     let mut ss = ServiceSpecification::new("Access");
@@ -18,5 +23,15 @@ fn main() {
 
     let ps = ProductSpecification::from(ss);
 
+    let mut so = ServiceOrder::new();
+    so.note.as_mut().unwrap().push(Note::new("This is a Note."));
+    let ind = Individual::new("John Q. Citizen");
+    so.related_party.as_mut().unwrap().push(RelatedParty::from(&ind));
+    
+    so.external_id = Some("PON1234983".to_string());
+    let po = ProductOrder::from(so);
+
     dbg!(ps);
+
+    dbg!(po);
 }
