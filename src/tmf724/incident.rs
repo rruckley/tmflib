404c4fb5ba75a60050f7192dbfb1df92bb008a64
Ryan Ruckley <rruckley@gmail.com>
2024-02-02 11:25:35 +1100

    Complete payload

--- src/tmf724/incident.rs
+++ src/tmf724/incident.rs
@@ -2,13 +2,14 @@
 //! 
 
 use serde::{Deserialize,Serialize};
-use crate::{LIB_PATH,HasId,CreateTMF, HasName, Uri};
+use crate::{LIB_PATH,HasId,CreateTMF, HasName, Uri, DateTime};
 use tmflib_derive::{HasId,HasName};
 use super::MOD_PATH;
 const CLASS_PATH : &str = "incident";
 
 /// Incident Priority
 #[derive(Clone, Debug, Default, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
 pub enum PriorityType {
     /// Critical
     Critical,
@@ -23,6 +24,7 @@ pub enum PriorityType {
 
 /// Incident Urgency 
 #[derive(Clone, Debug, Default, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
 pub enum UrgencyType {
     /// Critical
     Critical,
@@ -37,6 +39,7 @@ pub enum UrgencyType {
 
 /// Incident Acknowledge State
 #[derive(Clone, Debug, Default, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
 pub enum IncidentAckStateType {
     /// Acknowledged
     Acknowledged,
@@ -47,6 +50,7 @@ pub enum IncidentAckStateType {
 
 /// Incident Status
 #[derive(Clone, Debug, Default, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
 pub enum IncidentStateType {
     /// Raised
     #[default]
@@ -59,6 +63,7 @@ pub enum IncidentStateType {
 
 /// Incident Impact
 #[derive(Clone, Debug, Default, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
 pub enum ImpactType {
     /// Extensive [Highest]
     Extensive,
@@ -71,17 +76,123 @@ pub enum ImpactType {
     Minor,
 }
 
+/// Incident Impact
+#[derive(Clone, Debug, Default, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Characteristic {
+    /// Unique Id
+    pub id : String,
+    /// Name
+    pub name : String,
+    /// Value
+    pub value : String,
+    /// Value type
+    pub value_type: String,
+}
+
+/// Reference to an external reource
+#[derive(Clone, Debug, Default, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ResourceEntity {
+    /// Referenced Uri
+    pub href: Option<Uri>,
+    /// Referenced Id
+    pub id: Option<String>,
+}
+/// Root Cause
+#[derive(Clone, Debug, Default, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RootCause {
+    href : Uri,
+    id : String,
+    location: String,
+}
+
+/// Entity Reference
+#[derive(Clone, Debug, Default, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct EntityRef {
+    href: Uri,
+    id : String,
+    name: String,
+}
+
+/// External Identifier Reference
+#[derive(Clone, Debug, Default, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ExternalIdentifier {
+    external_identifier_type: String,
+    href: Uri,
+    id: String,
+    owner: String,
+}
+
 /// ITIL Incident
 #[derive(Clone, Debug, Default, Deserialize, HasId, HasName, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Incident {
-    city: String,
-    country : String,
-    geographic_address_type: String,
+    /// Acknowledged Status
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub ack_state: Option<IncidentAckStateType>,
+    /// Acknowledged Time
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub ack_time: Option<DateTime>,
+    /// Incident Category
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub category: Option<String>,
+    /// Incident resolution time
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub clear_time: Option<DateTime>,
+    /// Domain
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub domain: Option<String>,
     /// Url to Incident
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub href: Option<Uri>,
     /// Id for incident
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub id: Option<String>,
+    /// What is the impact of this incident?
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub impact: Option<ImpactType>,
+    /// Detailed description of incident
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub incident_detail: Option<String>,
+    /// Suggested resolution for this incident
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub incident_resolution_suggestion: Option<String>,
     /// Incident Title
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub name: Option<String>,
+    /// Incident first occurance
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub occur_time: Option<DateTime>,
+    /// Priority
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub priority: Option<PriorityType>,
+    /// Status
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub state: Option<IncidentStateType>,
+    /// Last update time
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub update_time: Option<DateTime>,
+    /// Urgency
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub urgency: Option<UrgencyType>,
+
+    // Referenced types
+    /// Extensions
+    pub extension_info: Option<Vec<Characteristic>>,
+    /// Events
+    pub event_id: Option<Vec<ResourceEntity>>,
+    /// Root Event
+    pub root_event_id: Option<Vec<ResourceEntity>>,
+    /// Source Object
+    pub source_object: Option<Vec<ResourceEntity>>,
+    /// Root Cause(s)
+    pub root_cause: Option<Vec<RootCause>>,
+    /// Affected Entities
+    pub affected_entity: Option<Vec<EntityRef>>,
+    /// External Identifiers
+    pub external_identifier: Option<Vec<ExternalIdentifier>>,
 }
