659ba87eaa3c2b2bc05fcf1506a1cd80053d8ce3
Ryan Ruckley <rruckley@gmail.com>
2024-02-02 13:20:09 +1100

    expanded

--- examples/create_individual.rs
+++ examples/create_individual.rs
@@ -3,9 +3,11 @@
 use tmflib::tmf632::individual::Individual;
 
 fn main() {
-    let individual = Individual::new("John Smith")
+    let individual = Individual::new("John Bagford Smith")
         .email("john.smith@example.com")
-        .mobile("0411 111 111");
+        .mobile("0411 111 111")
+        .title("Mr")
+        .preferred("Baggie");
 
     dbg!(individual);
 }
