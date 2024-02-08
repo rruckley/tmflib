f173e2fb99bd9cd626e82e097baa7ed17b36b796
Ryan Ruckley <rruckley@gmail.com>
2024-02-08 12:24:12 +1100

    engaged party is mandatory

--- src/tmf669/party_role.rs
+++ src/tmf669/party_role.rs
@@ -3,6 +3,7 @@
 use serde::{Deserialize, Serialize};
 
 use crate::{HasId,HasName, CreateTMF,LIB_PATH, common::related_party::RelatedParty};
+use crate::tmf632::individual::Individual;
 use tmflib_derive::{HasId,HasName};
 
 use super::MOD_PATH;
@@ -25,20 +26,28 @@ pub struct PartyRole {
 }
 
 impl PartyRole {
-    /// Create new PartyRole with given name
-    pub fn new(name : impl Into<String>) -> PartyRole {
+    /// Create new PartyRole based on a given [Individual].
+    /// ```
+    /// # use tmflib::tmf669::party_role::PartyRole;
+    /// use tmflib::tmf632::individual::Individual;
+    /// let individual = Individual::new("John Smith");
+    /// let role = PartyRole::new("Account Manager",&individual);
+    /// ```
+    pub fn new(name : impl Into<String>,individual : &Individual) -> PartyRole {
+        
         let mut role = PartyRole::create();
         role.name = Some(name.into());
+        role.engaged_party = Some(RelatedParty::from(individual));
         role
     }
 
-    /// Set engaged party
+    /// Set engaged party (Using [RelatedParty] reference)
     pub fn engaged_party(mut self, related_party: RelatedParty) -> PartyRole {
         self.engaged_party = Some(related_party);
         self
     }
 
-    /// Add a related party
+    /// Add a [RelatedParty] to the PartyRole
     pub fn add_party(&mut self, party : RelatedParty) {
         self.related_party.push(party);
     }
