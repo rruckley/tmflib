ca1998d733f8461829d90762b533da1ff8e2ba76
Ryan Ruckley <rruckley@gmail.com>
2024-02-22 16:59:15 +1100

    cleanup

--- examples/create_tmf.rs
+++ examples/create_tmf.rs
@@ -8,19 +8,19 @@
 include!(concat!(env!("OUT_DIR"), "/auto-lib.rs"));
 
 // This module and struct are auto-generated
-use tmf723::policy_rule::PolicyRule;
+//use tmf723::policy_rule::PolicyRule;
 
 use openapiv3::OpenAPI;
 
 fn main() {
-    let rule = PolicyRule {
-        default_0: "A Field".to_string(),
-    };
+    // let rule = PolicyRule {
+    //     default_0: "A Field".to_string(),
+    // };
     println!("There is a message about auto-generation");
 
     // Open a OAS file
-    let data = include_str!("../open_api/TMF723-Policy_Management-v5.0.0.oas.json");
-    let openapi : OpenAPI = serde_json::from_str(data).expect("Could not parse YAML");
+    //let data = include_str!("../open_api/TMF723-Policy_Management-v5.0.0.oas.json");
+    //let openapi : OpenAPI = serde_json::from_str(data).expect("Could not parse YAML");
 
     //dbg!(openapi);
 }
