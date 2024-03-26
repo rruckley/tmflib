4d66976751f14ac0fb99dc813ec6131f17a28992
Ryan Ruckley <rruckley@gmail.com>
2024-03-26 13:44:57 +1100

    shopping cart into product order

--- src/tmf622/product_order_v4.rs
+++ src/tmf622/product_order_v4.rs
@@ -8,12 +8,14 @@ use crate::common::related_party::RelatedParty;
 use crate::common::note::Note;
 use crate::tmf651::agreement::AgreementRef;
 use crate::{CreateTMF, CreateTMFWithTime, DateTime, HasId, HasLastUpdate, HasNote, HasRelatedParty};
+use crate::tmf663::shopping_cart::ShoppingCart;
+use super::product_order_item::ProductOrderItem;
 
 // URL Path components
 use super::LIB_PATH;
 use super::MOD_PATH;
 
-use super::product_order_item::ProductOrderItem;
+
 
 const CLASS_PATH: &str = "productOrder";
 
@@ -138,5 +140,27 @@ impl From<ServiceOrder> for ProductOrder {
         po.product_order_item = items;
 
         po  
+    }
+}
+
+impl From<ShoppingCart> for ProductOrder {
+    fn from(value: ShoppingCart) -> Self {
+        // Convert a Shopping cart into a product order.
+        // Each CartItem converts into an order item using a conversion function.
+        let mut order = ProductOrder::new();
+        order.description = Some("Order from Cart".into());
+        // Bring across the cart items
+        if value.cart_item.is_some() {
+            value.cart_item.unwrap().into_iter().for_each(|i| {
+                order.product_order_item.as_mut().unwrap().push(ProductOrderItem::from(i));
+            });
+        }
+        // Bring across the related parties
+        if value.related_party.is_some() {
+            value.related_party.unwrap().into_iter().for_each(|rp| {
+                order.related_party.as_mut().unwrap().push(rp.clone());
+            });
+        }
+        order
     }
 }
