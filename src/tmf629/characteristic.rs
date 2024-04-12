4f6e1fe241e6cb796b31601e00e4f6ed07abdc04
Ryan Ruckley <rruckley@gmail.com>
2024-04-12 10:45:38 +1000

    create fns

--- src/tmf629/characteristic.rs
+++ src/tmf629/characteristic.rs
@@ -12,3 +12,21 @@ pub struct Characteristic {
     /// Value of characteristic
     pub value: String,
 }
+
+impl Characteristic {
+    /// Create a new characteristic from name / value
+    pub fn new(name : impl Into<String>, value : impl Into<String>) -> Characteristic {
+        Characteristic {
+            name : name.into(),
+            value: value.into(),
+            value_type : String::from("String"),
+        }
+    }
+}
+
+impl From<(&str, &str)> for Characteristic {
+    fn from(value: (&str, &str)) -> Self {
+        let (name,value) = value;
+        Characteristic::new(name,value)
+    }
+}
