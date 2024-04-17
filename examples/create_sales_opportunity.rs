a6fb2cda6cb3e0b7f51867a1a7726a10e7deca02
Ryan Ruckley <rruckley@gmail.com>
2024-04-17 12:04:28 +1000

    bigger example

--- examples/create_sales_opportunity.rs
+++ examples/create_sales_opportunity.rs
@@ -2,10 +2,18 @@
 //! 
 
 use tmflib::tmf699::sales_opportunity_v5::SalesOpportunity;
+use tmflib::tmf699::sales_opportunity_item_v5::SalesOpportunityItem;
+use tmflib::tmf629::customer::Customer;
+use tmflib::HasName;
 
 fn main() {
+    let mut cust = Customer::default();
+    cust.set_name("A Customer");
+    let item= SalesOpportunityItem::new()
+        .for_customer(cust);    
     let opp = SalesOpportunity::new("My Opportunity")
-        .description("This is a new opportunity");
+        .description("This is a new opportunity")
+        .item(item);
 
     dbg!(opp);
 }
