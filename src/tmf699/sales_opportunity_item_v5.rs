0786d5f20a5ed369d1cd59972bd580da9f93a4d2
Ryan Ruckley <rruckley@gmail.com>
2024-04-17 12:05:51 +1000

    supporting fns

--- src/tmf699/sales_opportunity_item_v5.rs
+++ src/tmf699/sales_opportunity_item_v5.rs
@@ -2,7 +2,13 @@
 
 use serde::{Deserialize,Serialize};
 
-use crate::{common::{money::Money, note::Note, related_party::RelatedParty}, TimePeriod};
+use crate::{common::{
+    money::Money, 
+    note::Note, 
+    related_party::RelatedParty
+}, TimePeriod
+};
+use crate::tmf629::customer::Customer;
 
 use super::{sales_lead_v5::SalesLeadRef, sales_opportunity_v5::SalesOpportunityPriorityType};
 
@@ -17,8 +23,26 @@ pub struct SalesOpportunityItem {
     #[serde(skip_serializing_if = "Option::is_none")]
     estimated_revenue: Option<Money>,
     /// Notes
-    pub note : Vec<Note>,  
-    related_party : Vec<RelatedParty>,
+    pub note : Vec<Note>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    related_party : Option<Vec<RelatedParty>>,
     #[serde(skip_serializing_if = "Option::is_none")]
     sales_lead: Option<SalesLeadRef>,
+}
+
+impl SalesOpportunityItem {
+    /// Create a new Opportunity Item
+    pub fn new() -> SalesOpportunityItem {
+        SalesOpportunityItem {
+            valid_for: Some(TimePeriod::period_30days()),
+            related_party: Some(vec![]),
+            ..Default::default()
+        }
+    }
+
+    /// Add customer to opportunity item
+    pub fn for_customer(mut self, cust : Customer) -> SalesOpportunityItem {
+        self.related_party.as_mut().unwrap().push(RelatedParty::from(&cust));
+        self
+    }
 }
