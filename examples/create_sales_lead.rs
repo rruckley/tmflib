d569233c86e7b24d71b475d0031f693b55c20a98
Ryan Ruckley <rruckley@gmail.com>
2024-04-17 12:53:35 +1000

    fix v5 uses

--- examples/create_sales_lead.rs
+++ examples/create_sales_lead.rs
@@ -1,6 +1,6 @@
 //! Test Sales LEad
 
-#[cfg(feature = "tmf699_v4")]
+#[cfg(feature = "tmf699-v4")]
 use tmflib::tmf699::sales_lead_v4::SalesLead;
 #[cfg(feature = "tmf699-v5")]
 use tmflib::tmf699::sales_lead_v5::SalesLead;
--- examples/create_sales_opportunity.rs
+++ examples/create_sales_opportunity.rs
@@ -1,7 +1,9 @@
 //! Create Sales Opportunity
 //! 
 
+#[cfg(feature ="tmf699-v5")]
 use tmflib::tmf699::sales_opportunity_v5::SalesOpportunity;
+#[cfg(feature = "tmf699-v5")]
 use tmflib::tmf699::sales_opportunity_item_v5::SalesOpportunityItem;
 use tmflib::tmf629::customer::Customer;
 use tmflib::HasName;
@@ -9,11 +11,14 @@ use tmflib::HasName;
 fn main() {
     let mut cust = Customer::default();
     cust.set_name("A Customer");
+    #[cfg(feature ="tmf699-v5")]
     let item= SalesOpportunityItem::new()
-        .for_customer(cust);    
+        .for_customer(cust);
+    #[cfg(feature ="tmf699-v5")]
     let opp = SalesOpportunity::new("My Opportunity")
         .description("This is a new opportunity")
         .item(item);
 
+    #[cfg(feature ="tmf699-v5")]
     dbg!(opp);
 }
