e5c70f88ae04987c7b7fd5e766ebc0b7ee7a5551
Ryan Ruckley <rruckley@gmail.com>
2024-05-27 15:23:04 +1000

    added new()

--- src/tmf697/v4/work_order.rs
+++ src/tmf697/v4/work_order.rs
@@ -4,22 +4,76 @@ use crate::{
     LIB_PATH,
     HasId,
     CreateTMF,
+    HasNote,
     Uri,
 };
 use super::{work_order_item::WorkOrderItem, MOD_PATH};
-use tmflib_derive::HasId;
+use crate::common::note::Note;
+use tmflib_derive::{HasId,HasNote};
 use serde::{Deserialize,Serialize};
 
 const CLASS_PATH : &str = "workorder";
 
+/// Work Order States
+#[derive(Clone,Debug,Default,Deserialize,Serialize)]
+pub enum WorkOrderStateType {
+    #[default]
+    /// Acknowledged
+    Acknowledged,
+    /// WO is rejected
+    Rejected,
+    /// WO is pending an action
+    Pending,
+    /// WO is held
+    Held,
+    /// WO is in progress
+    InProgress,
+    /// WO has been cancelled
+    Cancelled,
+    /// WO has been completed
+    Completed,
+    /// WO has failed
+    Failed,
+    /// WO has been partially completed
+    Partial,
+    /// WO is under accessment for cancellation
+    AccessingCancellation,
+    /// WO is awaiting cancellation
+    PendingCancellation,
+}
+
 /// Work Order
-#[derive(Clone,Debug,Default,Deserialize,HasId,Serialize)]
+#[derive(Clone,Debug,Default,Deserialize,HasId,HasNote,Serialize)]
 pub struct WorkOrder {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(rename = "@type")]
     r#type: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(rename = "@baseType")]
     base_type: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(rename = "@schemaLocation")]
     schema_location: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     href: Option<Uri>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     id: Option<String>,
+    /// Work Order Status@type
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub state: Option<WorkOrderStateType>,
     // Referenced structures
-    work_order_item : Vec<WorkOrderItem>,
+    /// Work Order Items
+    pub work_order_item : Vec<WorkOrderItem>,
+    /// Work Order Notes
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub note: Option<Vec<Note>>,
+}
+
+impl WorkOrder {
+    /// Create a new Work Order instance
+    pub fn new() -> WorkOrder {
+        let mut out = WorkOrder::create();
+        out.state = Some(WorkOrderStateType::default());
+        out
+    }
 }
