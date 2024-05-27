0a26280e51333845338b772ceb1d41c2c96c69b8
Ryan Ruckley <rruckley@gmail.com>
2024-05-27 15:20:57 +1000

    add meta-field mapping

--- src/tmf697/v4/work_order_item.rs
+++ src/tmf697/v4/work_order_item.rs
@@ -7,8 +7,15 @@ use serde::{Deserialize,Serialize};
 /// Work Order Item
 #[derive(Clone,Debug,Default,Deserialize,Serialize)]
 pub struct WorkOrderItem {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(rename = "@type")]
     r#type: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(rename = "@baseType")]
     base_type: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(rename = "@schemaLocation")]
     schema_location: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     id: Option<String>,
 }
