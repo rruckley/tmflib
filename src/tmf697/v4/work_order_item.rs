6601e995c46dfdd3c8b074487f234de8493cc1e4
Ryan Ruckley <rruckley@gmail.com>
2024-05-28 13:37:36 +1000

    added new() fn

--- src/tmf697/v4/work_order_item.rs
+++ src/tmf697/v4/work_order_item.rs
@@ -3,19 +3,37 @@
 //! 
 
 use serde::{Deserialize,Serialize};
+use super::work::WorkRefOrValue;
 
 /// Work Order Item
 #[derive(Clone,Debug,Default,Deserialize,Serialize)]
 pub struct WorkOrderItem {
+    /// Metadata: Type of schema, same as [`base_type`] if aligned to TMF specification.
     #[serde(skip_serializing_if = "Option::is_none")]
     #[serde(rename = "@type")]
-    r#type: Option<String>,
+    pub r#type: Option<String>,
+    /// Metadata: Parent type of schema for derived types
     #[serde(skip_serializing_if = "Option::is_none")]
     #[serde(rename = "@baseType")]
-    base_type: Option<String>,
+    pub base_type: Option<String>,
+    /// Metadata: Location of schema if derived schema used, i.e. @type differs from @base_type
     #[serde(skip_serializing_if = "Option::is_none")]
     #[serde(rename = "@schemaLocation")]
-    schema_location: Option<String>,
+    pub schema_location: Option<String>,
+    /// Unique Id
     #[serde(skip_serializing_if = "Option::is_none")]
-    id: Option<String>,
+    pub id: Option<String>,
+    /// Reference of Value for Work
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub work: Option<WorkRefOrValue>,
 }
+
+impl WorkOrderItem {
+    /// Create new WorkOrderItem based on some Work
+    pub fn with(work : WorkRefOrValue) -> WorkOrderItem {
+        let mut woi = WorkOrderItem::default();
+        woi.work = Some(work);
+        woi
+    }
+}
+
