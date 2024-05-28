094dc63e3b9fb240522c8d9cc4ee4e03ffb22cbe
Ryan Ruckley <rruckley@gmail.com>
2024-05-28 13:37:20 +1000

    add work and woi

--- examples/create_work_order.rs
+++ examples/create_work_order.rs
@@ -1,10 +1,23 @@
 //! Create Work Order Example
 
 use tmflib::tmf697::v4::work_order::WorkOrder;
+use tmflib::tmf697::v4::work::{
+    Work,
+    WorkRefOrValue,
+};
+use tmflib::tmf697::v4::work_order_item::WorkOrderItem;
 
 fn main() {
 
-    let wo = WorkOrder::new();
+    let mut wo = WorkOrder::new();
 
-    dbg!(wo);
+    let work = Work::new("Some work");
+
+    let woi = WorkOrderItem::with(WorkRefOrValue::from(work));
+
+    wo.add_item(woi);
+
+    // dbg!(wo);
+    let json = serde_json::to_string(&wo).unwrap();
+    println!("JSON>>\n{}\n<<JSON",json);
 }
