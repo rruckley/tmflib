11adb2af41627cc8cc70c3132aa4d15ba3d7aa1b
Ryan Ruckley <rruckley@gmail.com>
2024-06-18 15:32:52 +1000

    add characteristics

--- src/tmf696/mod.rs
+++ src/tmf696/mod.rs
@@ -4,5 +4,6 @@
 pub mod product_order_risk_assessment;
 pub mod shopping_cart_risk_assessment;
 pub mod risk_assessment_result;
+pub mod characteristic;
 
 const MOD_PATH : &str = "riskManagement/v4";
