ed4738c29211f2129f307c35563b15555620aaab
Ryan Ruckley <rruckley@gmail.com>
2024-06-18 15:35:55 +1000

    add characteristic

--- examples/create_risk_assessment.rs
+++ examples/create_risk_assessment.rs
@@ -1,12 +1,15 @@
 //! Create Risk Assessment Example
 
 use tmflib::tmf696::product_order_risk_assessment::ProductOrderRiskAssessment;
+use tmflib::tmf696::characteristic::Characteristic;
 use tmflib::tmf622::product_order_v4::{ProductOrder,ProductOrderRef};
 
 fn main() {
     let mut order = ProductOrder::new();
     order.description = Some("A Customer Order".to_string());
-    let risk = ProductOrderRiskAssessment::new(ProductOrderRef::from(&order));
+    let mut risk = ProductOrderRiskAssessment::new(ProductOrderRef::from(&order));
+    let char = Characteristic::new("name","value");
+    risk.replace_characteristic(char);
 
     dbg!(risk);
 }
