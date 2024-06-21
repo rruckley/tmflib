94a4a39a4d368c2220431ea2d159123ea47140e8
Ryan Ruckley <rruckley@gmail.com>
2024-06-21 17:20:20 +1000

    add message and signatur

--- examples/create_shipping_order.rs
+++ examples/create_shipping_order.rs
@@ -1,6 +1,7 @@
 //! Create Shipping Order Example
 //! 
 
+use tmflib::tmf700::shipping_instruction::SignatureRequiredByType;
 use tmflib::tmf700::{shipping_order::ShippingOrder,shipping_order_item::ShippingOrderItem,shipping_instruction::ShippingInstruction};
 use tmflib::common::note::Note;
 use tmflib::HasNote;
@@ -8,7 +9,12 @@ use tmflib::HasNote;
 fn main() {
     let item = ShippingOrderItem::new()
         .instruction(ShippingInstruction::new("An Instruction"));
-    let mut shipping_order = ShippingOrder::new();
+    let mut shipping_order = ShippingOrder::new()
+        .instruction(
+            ShippingInstruction::new("Top Level Instructions")
+            .message("Please use the left dock")
+            .signature_required_by(Some(SignatureRequiredByType::Receiver))
+    );
     shipping_order.add_item(item);
     shipping_order.add_note(Note::new("A Note"));
 
