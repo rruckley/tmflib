8953772fc88fa7f61b09cac8e3610b235192bdbb
Ryan Ruckley <rruckley@gmail.com>
2024-07-02 09:59:26 +1000

    Remove old traits

--- src/tmf697/v4/work.rs
+++ src/tmf697/v4/work.rs
@@ -4,7 +4,6 @@ use crate::{
     LIB_PATH,
     HasId,
     HasName,
-    CreateTMF,
     TimePeriod,
     Uri,
 };
--- src/tmf697/v4/work_order.rs
+++ src/tmf697/v4/work_order.rs
@@ -3,7 +3,6 @@
 use crate::{
     LIB_PATH,
     HasId,
-    CreateTMF,
     HasNote,
     HasRelatedParty,
     Uri,
