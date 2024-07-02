d3027cfd14ba3632d68b658da208ee679d300e3e
Ryan Ruckley <rruckley@gmail.com>
2024-07-02 12:45:24 +1000

    add new structs

--- src/tmf687/product_stock.rs
+++ src/tmf687/product_stock.rs
@@ -19,9 +19,33 @@ use tmflib_derive::{
 };
 use crate::common::related_party::RelatedParty;
 use crate::common::related_place::RelatedPlaceRefOrValue;
+use crate::common::product::ProductRefOrValue;
 use serde::{Deserialize,Serialize};
 
 const CLASS_PATH : &str = "productStock";
+
+#[derive(Clone,Default,Debug,Deserialize,Serialize)]
+pub struct ProductStockRelationship {
+    relationship_type : String,
+
+}
+
+#[derive(Clone,Default,Debug,Deserialize,Serialize)]
+pub struct ProductStockRef {
+    href: Uri,
+    id: String,
+    name: String,
+}
+
+impl From<ProductStock> for ProductStockRef {
+    fn from(value: ProductStock) -> Self {
+        ProductStockRef {
+            id: value.get_id(),
+            href: value.get_href(),
+            name: value.get_name(),
+        }
+    }
+}
 
 /// Product Stock Record
 #[derive(Clone,Default,Debug,Deserialize,HasId,HasName,HasLastUpdate,HasRelatedParty,Serialize)]
@@ -40,4 +64,16 @@ pub struct ProductStock {
     related_party: Option<Vec<RelatedParty>>,
     #[serde(skip_serializing_if = "Option::is_none")]
     place: Option<Vec<RelatedPlaceRefOrValue>>,
+    stocked_product: ProductRefOrValue,
+    product_stock_relationship: Option<Vec<ProductStockRelationship>>,
+}
+
+impl ProductStock {
+    /// Create a new ProductStock instance
+    pub fn new(name : impl Into<String>) -> ProductStock {
+        ProductStock {
+            name: Some(name.into()),
+            ..ProductStock::create()
+        }
+    }
 }
