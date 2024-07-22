53ef6af934bdf05ef3af4f63903665e7991723e5
Ryan Ruckley <rruckley@gmail.com>
2024-07-22 16:39:46 +1000

    clippy

--- src/tmf622/product_order_v5.rs
+++ src/tmf622/product_order_v5.rs
@@ -52,12 +52,9 @@ pub struct ProductOrderRef {
 
 impl From<&ProductOrder> for ProductOrderRef {
     fn from(value: &ProductOrder) -> Self {
-        let name = match value.description.as_deref() {
-            Some(d) => d,
-            None => {
-                "No Order Description"
-            }
-        };
+        let name = value.description.as_deref().unwrap_or({
+            "No Order Description"
+        });
         ProductOrderRef {
             href: value.get_href(),
             id: value.get_id(),
