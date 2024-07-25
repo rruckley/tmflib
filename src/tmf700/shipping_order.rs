46b0c41faedab0d6be3678ac374c7a8a14cadfff
Ryan Ruckley <rruckley@gmail.com>
2024-07-25 10:47:47 +1000

    UnitTests

--- src/tmf700/shipping_order.rs
+++ src/tmf700/shipping_order.rs
@@ -76,7 +76,7 @@ pub struct ShippingOrder {
     // Referenced Types
     //
     /// Shipping Line Items
-    pub shipping_order_item: Vec<ShippingOrderItem>,
+    pub shipping_order_item: Option<Vec<ShippingOrderItem>>,
     /// Notes
     #[serde(skip_serializing_if = "Option::is_none")]
     pub note: Option<Vec<Note>>,
@@ -94,9 +94,7 @@ pub struct ShippingOrder {
 impl ShippingOrder {
     /// Create new ShippingOrder
     pub fn new() -> ShippingOrder {
-        let mut order = ShippingOrder::create();
-        order.note = Some(vec![]);
-        order
+        ShippingOrder::create()
     }
 
     /// Set shipping instructions for this shipping order
@@ -107,7 +105,10 @@ impl ShippingOrder {
 
     /// Add an order item to this order
     pub fn add_item(&mut self, item : ShippingOrderItem) {
-        self.shipping_order_item.push(item);
+        match self.shipping_order_item.as_mut() {
+            Some(v) => v.push(item),
+            None => self.shipping_order_item = Some(vec![item]),
+        }
     }
 
     /// Add a RelatedShippingOrder to this order
@@ -118,6 +119,7 @@ impl ShippingOrder {
 #[cfg(test)]
 mod test {
     use crate::tmf700::shipping_instruction::ShippingInstruction;
+    use crate::tmf700::shipping_order_item::ShippingOrderItem;
 
     use super::RelatedShippingOrder;
     use super::ShippingOrder;
@@ -172,5 +174,19 @@ mod test {
 
         assert_eq!(so.shipping_instruction.is_some(),true);
         assert_eq!(so.shipping_instruction.unwrap().label_message,Some(SHIP_INST.to_string()));
+    }
+
+    #[test]
+    fn shipping_order_add_item() {
+        let mut so = ShippingOrder::new();
+        let item1 = ShippingOrderItem::new();
+        so.add_item(item1);
+
+        assert_eq!(so.shipping_order_item.is_some(),true);
+
+        let item2 = ShippingOrderItem::new();
+        so.add_item(item2);
+
+        assert_eq!(so.shipping_order_item.unwrap().len(),2);        
     }
 }
