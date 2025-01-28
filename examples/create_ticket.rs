f98e742f9bbc36b639aa8b1c1964b16c90b01b0c
Ryan Ruckley <rruckley@gmail.com>
2025-01-29 10:23:48 +1100

    bug: Fix example with conditional compile

--- examples/create_ticket.rs
+++ examples/create_ticket.rs
@@ -1,11 +1,15 @@
 //! Create Ticket Example
 
+#[cfg(feature = "tmf621")]
 use tmflib::{tmf621::trouble_ticket::TroubleTicket, HasDescription};
 
 fn main() {
-    let ticket = TroubleTicket::new("A Ticket")
-    .description("This is a trouble ticket");
-
-
-    dbg!(ticket);
+    #[cfg(feature = "tmf621")]
+    {
+        let ticket = TroubleTicket::new("A Ticket")
+        .description("This is a trouble ticket");
+    
+    
+        dbg!(ticket);
+    }
 }
