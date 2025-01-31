34314fe6bf143d0784365dd95778c887f1f6e788
Ryan Ruckley <rruckley@gmail.com>
2025-01-31 16:26:34 +1100

    feat: Add related party

--- src/tmf645/check_service_qualification.rs
+++ src/tmf645/check_service_qualification.rs
@@ -3,29 +3,35 @@
 use serde::{Deserialize,Serialize};
 
 use crate::{
-    HasId,
-    HasDescription,
-    LIB_PATH,
-    Uri
+    common::related_party::RelatedParty, HasDescription, HasId, HasRelatedParty, Uri, LIB_PATH
 };
 
 use tmflib_derive::{
     HasId,
     HasDescription,
+    HasRelatedParty,
 };
 
 const CLASS_PATH : &str = "checkServiceQualification";
 use super::MOD_PATH;
 
 /// Check Service Qualification
-#[derive(Clone,Debug,Default,HasId,HasDescription,Deserialize,Serialize)]
+#[derive(Clone,Debug,Default,HasId,HasDescription,HasRelatedParty, Deserialize,Serialize)]
+#[serde(rename_all = "camelCase")]
 pub struct CheckServiceQualificaitonItem {
     /// Unique Id
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub id : Option<String>,
     /// HTTP URI
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub href: Option<Uri>,
     /// Description
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub description : Option<String>,
+    // Linked Objects
+    /// Related Parties
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub related_party : Option<Vec<RelatedParty>>,
 }
 
 /// Check Service Qualification 
