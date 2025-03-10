1acea8dcd10e7445d3ea775f93b95938dcd7f796
Ryan Ruckley <rruckley@gmail.com>
2025-03-10 12:47:00 +1100

    feat: TMF921

--- src/tmf921/intent.rs
+++ src/tmf921/intent.rs
@@ -1,7 +1,7 @@
 
 //! Intent Module
 
-use crate::{TimePeriod,HasId,HasName,HasDescription,LIB_PATH};
+use crate::{DateTime, HasDescription, HasId, HasName, TimePeriod, LIB_PATH};
 use serde::{Deserialize, Serialize};
 use tmflib_derive::{HasName,HasId,HasDescription};
 use super::MOD_PATH;
@@ -20,20 +20,31 @@ const CLASS_PATH : &str = "intent";
 /// * `status` - The current status of the intent.
 /// * `valid_for` - An optional time period during which the intent is valid.
 #[derive(Clone, Debug, Default, Deserialize, Serialize, HasId,HasName, HasDescription)]
+#[serde(rename_all = "camelCase")]
 pub struct Intent {
     /// An optional unique identifier for the intent.
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub id: Option<String>,
     /// An optional hyperlink reference to the intent.
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub href: Option<String>,
     /// An optional name for the intent.
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub name: Option<String>,
     /// An optional description of the intent.
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub description: Option<String>,
-    /// The version of the intent.
-    pub version: String,
     /// The current status of the intent.
-    pub status: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub status: Option<String>,
+    /// The date and time when the status was last changed.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub status_change_date: Option<DateTime>,
     /// An optional time period during which the intent is valid.
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub valid_for: Option<TimePeriod>,
+    /// The version of the intent.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub version: Option<String>,
 }
 
