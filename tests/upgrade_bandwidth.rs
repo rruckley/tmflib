f51768f5c5c2c017b9ecdc8c697a6d71dad01459
Ryan Ruckley <ryan.ruckley@optus.com.au>
2025-07-22 12:50:47 +1000

    bug: fix test to get bandwidth characteristic

--- tests/upgrade_bandwidth.rs
+++ tests/upgrade_bandwidth.rs
@@ -5,19 +5,22 @@
 #[test]
 fn test_service_bandwidth_upgrade() {
     use tmflib::tmf638::service::{Characteristic,Service};
-    use tmflib::tmf641::service_order::ServiceOrder;
+    // use tmflib::tmf641::service_order::ServiceOrder;
 
-    // // Create a new service
-    // let mut service = Service::new("TestService")
-    //     .with_characteristic(Characteristic  {
-    //         name: "bandwidth".to_string(), 
-    //         value: "100".into(),
-    //         ..Default::default()
-    // });
+    // Create a new service
+    let service = Service::new("TestService")
+        .with_characteristic(
+            Characteristic::new(
+                "bandwidth".to_string(), 
+                100.into()
+            )
+        );
 
 
     // // Step 1 get current bandwidth
-    // let current_bandwidth = service.get_characteristics("bandwidth");
+    let current_bandwidth = service.get_characteristics("bandwidth");
+
+    assert_eq!(current_bandwidth.is_some(),true);
 
     // let service_order = 
     // // Check if the event is generated correctly
