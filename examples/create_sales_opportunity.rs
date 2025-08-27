22a5d6040dbe20dad1fd455e7eeac6bef79a7f90
Ryan Ruckley <ryan.ruckley@optus.com.au>
2025-08-27 16:28:52 +1000

    chore: cleanup

--- examples/create_sales_opportunity.rs
+++ examples/create_sales_opportunity.rs
@@ -5,8 +5,8 @@
 use tmflib::tmf699::sales_opportunity_item_v5::SalesOpportunityItem;
 #[cfg(all(feature = "tmf699", feature = "build-V5"))]
 use tmflib::tmf699::sales_opportunity_v5::SalesOpportunity;
-use tmflib::tmf629::customer::Customer;
-use tmflib::HasName;
+// use tmflib::tmf629::customer::Customer;
+// use tmflib::HasName;
 fn main() {
     #[cfg(all(feature = "tmf696", feature = "build-V5"))]
     {
