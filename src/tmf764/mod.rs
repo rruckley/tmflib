5da0c7baccc8ff8624aa934e3d34c7053c0dc74f
Ryan Ruckley <ryan.ruckley@optus.com.au>
2025-11-07 17:01:53 +1100

    feat: TMF764 CostRef

--- src/tmf764/mod.rs
+++ src/tmf764/mod.rs
@@ -16,5 +16,37 @@
 
 const MOD_PATH: &str = "costManagement/v5";
 
+use serde::{Deserialize, Serialize};
+
 pub mod actual_cost;
 pub mod projected_cost;
+
+use crate::Uri;
+
+/// Cost Reference
+#[derive(Clone, Debug, Default, Deserialize, PartialEq, Serialize)]
+pub struct CostRef {
+    /// Unique identifier
+    pub id: String,
+    /// Hyperlink reference
+    pub href: String,
+    /// Name
+    pub name: Option<String>,
+
+    /// Base Type this type is derived from if creating sub-classes
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(rename = "@baseType")]
+    pub base_type: Option<String>,
+    /// Schema Definition of the sub-class (if required)
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(rename = "@schemaLocation")]
+    pub schema_location: Option<Uri>,
+    /// Name for this Type when sub-classing
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(rename = "@type")]
+    pub r#type: Option<String>,
+    /// What type is this reference referring to?
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(rename = "@referredType")]
+    pub referred_type: Option<String>,
+}
