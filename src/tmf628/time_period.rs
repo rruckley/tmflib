6e2744c7bb6aa7f87d608e5b44cb5c0ca7d046f3
Ryan Ruckley <ryan.ruckley@optus.com.au>
2025-11-18 16:28:05 +1100

    Fix: Update to create DateTime

--- src/tmf628/time_period.rs
+++ src/tmf628/time_period.rs
@@ -1,7 +1,7 @@
 use serde::{Serialize, Deserialize};
 ///A period of time, either as a deadline (endDateTime only) a startDateTime only, or both
 #[derive(Debug, Clone, Serialize, Deserialize, Default)]
-pub struct TimePeriod {
+pub struct TimePeriodOld {
     ///End of the time period, using IETC-RFC-3339 format
     #[serde(rename = "endDateTime")]
     #[serde(default, skip_serializing_if = "Option::is_none")]
