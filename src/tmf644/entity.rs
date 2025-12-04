d0f781328ea7c798099ceb9aa7c678418c627b4b
Ryan Ruckley <ryan.ruckley@optus.com.au>
2025-12-05 10:14:54 +1100

    chore: Refactor to common

--- src/tmf644/entity.rs
+++
@@ -1,9 +0,0 @@
-use serde::{Serialize, Deserialize};
-///Base entity schema for use in TMForum Open-APIs. Property.
-#[derive(Debug, Clone, Serialize, Deserialize, Default)]
-pub struct Entity {}
-impl std::fmt::Display for Entity {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
-        write!(f, "{}", serde_json::to_string(self).unwrap())
-    }
-}
