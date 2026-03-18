5e8b9754a63798b134c2fa253e2dcc3e9df69456
Ryan Ruckley <ryan.ruckley@optus.com.au>
2026-03-18 17:52:33 +1100

    feat: IsAddressable Example

---
+++ examples/is_addressable.rs
@@ -0,0 +1,11 @@
+//! Show example using isAddreessable Trait
+//! 
+
+use tmflib::tmf632::individual_v4::Individual;
+use tmflib::IsAddressable;
+
+fn main() {
+    let objects = Individual::get_objects();
+    dbg!(objects);
+
+}
