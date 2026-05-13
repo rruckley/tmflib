b74f0e5808423abc8cbb476e2f6a30b929be008d
Ryan Ruckley <ryan.ruckley@optus.com.au>
2026-05-13 14:10:33 +1000

    chore: Clippy Pedantic

--- src/tmf727/party_ref_or_party_role_ref_fvo.rs
+++ src/tmf727/party_ref_or_party_role_ref_fvo.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
 
-/// Reference to a party or party role, which may be the related party or party role itself, or a reference to it. The referenced entity may be of any type, but is typically a Party or PartyRole.``
+/// Reference to a party or party role, which may be the related party or party role itself, or a reference to it. 
+/// The referenced entity may be of any type, but is typically a Party or PartyRole.
 #[derive(Debug, Clone, Serialize, Deserialize, Default)]
 pub struct PartyRefOrPartyRoleRefFvo {}
 impl std::fmt::Display for PartyRefOrPartyRoleRefFvo {
